aaadd0a02d2d7546bf52716e2a5050f27858b6a7
Stanislav Aleksandrovych <astanislav2361@gmail.com>
2021-05-18 16:41:25 +0300

    Class main improved

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -4,30 +4,46 @@ import mate.jdbc.dao.DriverDao;
 import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.CarService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    private static Injector injector = Injector.getInstance("mate.jdbc");
+    private final static Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
         ManufacturerDao manufacturerDao = (ManufacturerDao) injector
                 .getInstance(ManufacturerDao.class);
-        Car car = new Car("coolBMW", manufacturerDao.get(2L).get());
+        Manufacturer manufacturer = new Manufacturer("BMW", "Germany");
+        Car car = new Car("coolBMW", manufacturerDao.create(manufacturer));
+
         DriverDao driverDao = (DriverDao) injector.getInstance(DriverDao.class);
-        car.setDrivers(driverDao.getAll());
+        List<Driver> drivers = new ArrayList<>();
+        Driver firstDriver = new Driver("Johnathan", "1234321442");
+        Driver secondDriver = new Driver("Alice", "32958325");
+        drivers.add(driverDao.create(firstDriver));
+        drivers.add(driverDao.create(secondDriver));
+        car.setDrivers(drivers);
+
         CarService carService = (CarService) injector.getInstance(CarService.class);
-        System.out.println(carService.create(car));
+        Car createdCar = carService.create(car);
+        System.out.println(createdCar);
 
-        System.out.println(carService.get(9L));
+        System.out.println(carService.get(createdCar.getId()));
 
-        System.out.println(carService.delete(9L));
+        System.out.println(carService.delete(createdCar.getId()));
 
         System.out.println(carService.getAll());
 
-        Car updatedCar = new Car("notCoolAudi", manufacturerDao.get(1L).get());
-        updatedCar.setId(9L);
+        Manufacturer updatedManufacturer = new Manufacturer("Audi", "Germany");
+        manufacturerDao.create(updatedManufacturer);
+        Car updatedCar = new Car("notCoolAudi", updatedManufacturer);
+        updatedCar.setId(car.getId());
         System.out.println(carService.update(updatedCar));
 
-        System.out.println(carService.getAllByDriver(3L));
+        System.out.println(carService.getAllByDriver(firstDriver.getId()));
     }
 }
