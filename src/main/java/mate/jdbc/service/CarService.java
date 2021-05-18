9ccb6cc31d92b48f60252941cbae23ecc8954c6d
Veronika <koraf9712@gmail.com>
2021-05-18 14:28:24 +0300

    created Car, CarService, CarServiceImpl

---
+++ src/main/java/mate/jdbc/service/CarService.java
@@ -0,0 +1,22 @@
+package mate.jdbc.service;
+
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+
+public interface CarService {
+    Car create(Car car);
+
+    Car get(Long id);
+
+    List<Car> getAll();
+
+    Car update(Car car);
+
+    boolean delete(Long id);
+
+    void addDriverToCar(Driver driver, Car car);
+
+    List<Car> getAllByDriver(Long driverId);
+}
