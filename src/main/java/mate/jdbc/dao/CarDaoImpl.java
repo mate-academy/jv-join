c3c27232c1bb1bfa648cb40b205e956580c0e0cb
Kostiantyn <konkos2505@gmail.com>
2021-05-19 11:13:51 +0300

    optional refactored

--- src/main/java/mate/jdbc/dao/CarDaoImpl.java
+++ src/main/java/mate/jdbc/dao/CarDaoImpl.java
@@ -60,9 +60,8 @@ public class CarDaoImpl implements CarDao {
         }
         if (car != null) {
             car.setDrivers(getDriversForCar(id));
-            return Optional.of(car);
         }
-        return Optional.empty();
+        return Optional.ofNullable(car);
     }
 
     @Override
