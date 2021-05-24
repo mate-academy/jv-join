c83104b7999975b39a4fd7f767fa067c2145522c
Max Pochepets <agreed_note@hotmail.com>
2021-05-24 13:22:02 +0300

    fixed init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,11 +1,13 @@
-CREATE SCHEMA `taxi` DEFAULT CHARACTER SET utf8 ;
-CREATE TABLE `taxi`.`manufacturers` (
+USE `taxi`;
+DROP TABLE IF EXISTS `manufacturers`;
+
+CREATE TABLE `manufacturers` (
                                         `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
                                         `name` VARCHAR(225) NOT NULL,
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
-CREATE TABLE `taxi`.`drivers` (
+CREATE TABLE `drivers` (
                                   `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
                                   `name` VARCHAR(225) NOT NULL,
                                   `license_number` VARCHAR(225) NOT NULL,
