1a18557d4dc0a696fca38a7f8ad83e349401a611
Max Pochepets <agreed_note@hotmail.com>
2021-05-24 13:34:38 +0300

    dropping drivers table if exists

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,13 +1,15 @@
 CREATE SCHEMA IF NOT EXISTS `taxi` DEFAULT CHARACTER SET utf8;
 USE `taxi`;
-DROP TABLE IF EXISTS `manufacturers`;
 
+DROP TABLE IF EXISTS `manufacturers`;
 CREATE TABLE `manufacturers` (
                                         `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
                                         `name` VARCHAR(225) NOT NULL,
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
+DROP TABLE IF EXISTS `drivers`;
 CREATE TABLE `drivers` (
                                   `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
                                   `name` VARCHAR(225) NOT NULL,
