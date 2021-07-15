c6c0603d73f9cb2982e23b0285852e038d1b22db
Daniel Kuruch <kuruch1004@gmail.com>
2021-07-15 16:28:37 +0300

    refactored code

--- src/main/java/mate/jdbc/service/impl/CarServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/CarServiceImpl.java
@@ -20,9 +20,7 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public void addDriverToCar(Driver driver, Car car) {
-        List<Driver> drivers = car.getDrivers();
-        drivers.add(driver);
-        car.setDrivers(drivers);
+        car.getDrivers().add(driver);
         carDao.update(car);
     }
 
