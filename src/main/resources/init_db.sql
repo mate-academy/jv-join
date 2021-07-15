5bc0f80a0ac0ecb079b1047e72bc1486c22bd73c
Nikitozin <nikitozin91@gmail.com>
2021-07-15 19:33:21 +0300

    Implement_all_methods

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,26 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+DROP TABLE IF EXISTS `cars`;
+CREATE table `cars` (
+                        `id` BIGINT NOT NULL auto_increment,
+                        `model` VARCHAR(255) NOT NULL,
+                        `is_deleted` tinyint NOT NULL default 0,
+                        `manufacturer_id` BIGINT NOT NULL,
+                        primary KEY (`id`),
+                        CONSTRAINT `cars_manufacturers_fk`
+                                    FOREIGN KEY (`manufacturer_id`)
+                                            REFERENCES `taxi`.`manufacturers` (`id`)
+                                            ON DELETE NO ACTION
+                                            ON UPDATE NO ACTION
+);
+DROP TABLE IF EXISTS `cars_drivers`;
+CREATE table `cars_drivers` (
+                                `driver_id` BIGINT NOT NULL,
+                                `car_id` BIGINT NOT NULL,
+
+                                CONSTRAINT `cars_drivers_drivers_fk`
+                                    FOREIGN KEY (`driver_id`) references `drivers` (`id`),
+                                CONSTRAINT `cars_drivers_cars_fk`
+                                    FOREIGN KEY (`car_id`) references `cars` (`id`)
+);
