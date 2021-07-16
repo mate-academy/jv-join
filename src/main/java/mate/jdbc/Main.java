9d6fa8224d1cd2e4c802c65128849fc8e373dcf4
Veronika-Alekseienko <alekseienko.n@gmail.com>
2021-07-16 16:40:42 +0300

    added solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,85 @@
 package mate.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector =
+            Injector.getInstance("mate.jdbc");
+    private static final CarService carService =
+            (CarService) injector.getInstance(CarService.class);
+    private static final DriverService driverService =
+            (DriverService) injector.getInstance(DriverService.class);
+    private static final ManufacturerService manufacturerService =
+            (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
     public static void main(String[] args) {
-        // test your code here
+        Manufacturer bmw = new Manufacturer();
+        bmw.setName("BMW");
+        bmw.setCountry("Germany");
+
+        manufacturerService.create(bmw);
+
+        Driver veronika = new Driver();
+        veronika.setName("Veronika");
+        veronika.setLicenseNumber("1234");
+
+        Driver maksim = new Driver();
+        maksim.setName("Maksim");
+        maksim.setLicenseNumber("1234");
+
+        System.out.println("Add driver to db...");
+        driverService.create(veronika);
+
+        System.out.println("Add driver to db...");
+        driverService.create(maksim);
+        List<Driver> drivers = new ArrayList<>();
+        drivers.add(veronika);
+        System.out.println("Drivers was added...");
+
+        Car bmwX3 = new Car();
+        bmwX3.setModel("X3");
+        bmwX3.setManufacturer(bmw);
+        bmwX3.setDrivers(drivers);
+
+        Car bmwX5 = new Car();
+        bmwX5.setModel("X5");
+        bmwX5.setManufacturer(bmw);
+        bmwX5.setDrivers(drivers);
+
+        System.out.println("Add car to db...");
+        carService.create(bmwX3);
+
+        System.out.println("Add car to db...");
+        carService.create(bmwX5);
+
+        System.out.println("Get car from db...");
+        System.out.println(carService.get(bmwX3.getId()));
+
+        System.out.println("Get cars by driver...");
+        System.out.println(carService.getAllByDriver(veronika.getId()));
+
+        System.out.println("Get all cars...");
+        carService.getAll().forEach(System.out::println);
+
+        System.out.println("Update car in db...");
+        bmwX3.setModel("Old X3");
+        carService.update(bmwX3);
+
+        System.out.println("Delete car from db...");
+        carService.delete(bmwX3.getId());
+
+        System.out.println("Add driver to car...");
+        carService.addDriverToCar(maksim, bmwX5);
+
+        System.out.println("Remove driver from car...");
+        carService.removeDriverFromCar(maksim, bmwX5);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,20 +1,38 @@
 CREATE SCHEMA IF NOT EXISTS `taxi` DEFAULT CHARACTER SET utf8;
 USE `taxi`;
 
+DROP TABLE IF EXISTS `cars_drivers`;
+DROP TABLE IF EXISTS `cars`;
 DROP TABLE IF EXISTS `manufacturers`;
+DROP TABLE IF EXISTS `drivers`;
+
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                        `id` BIGINT NOT NULL AUTO_INCREMENT,
                                         `name` VARCHAR(225) NOT NULL,
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
 
-DROP TABLE IF EXISTS `drivers`;
 CREATE TABLE `drivers` (
-                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+								  `id` BIGINT NOT NULL AUTO_INCREMENT,
                                   `name` VARCHAR(225) NOT NULL,
                                   `license_number` VARCHAR(225) NOT NULL,
                                   `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                  PRIMARY KEY (`id`),
-                                  UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
-                                  UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+                                  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `cars` (
+								`id` BIGINT NOT NULL AUTO_INCREMENT,
+                                `model` VARCHAR(255) NOT NULL,
+                                `manufacturer_id` BIGINT,
+                                `is_deleted` TINYINT DEFAULT 0,
+                                PRIMARY KEY(`id`),
+                                FOREIGN KEY (`manufacturer_id`) REFERENCES manufacturers (`id`)
+);
+
+CREATE TABLE `cars_drivers` (
+								`car_id` BIGINT,
+                                `driver_id` BIGINT,
+                                FOREIGN KEY(`car_id`) REFERENCES cars(`id`),
+								FOREIGN KEY(`driver_id`) REFERENCES drivers(`id`)
+);
