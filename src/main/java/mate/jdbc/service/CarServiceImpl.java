255fb0fc967a0c9683361428474b4e2a13e8df53
sSlonIk <kryachok@gmail.com>
2021-07-16 09:16:48 +0300

    created Dao and services

---
+++ src/main/java/mate/jdbc/service/CarServiceImpl.java
@@ -0,0 +1,57 @@
+package mate.jdbc.service;
+
+import mate.jdbc.dao.CarDao;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.lib.Service;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+    @Inject
+    private CarDao carDao;
+
+    @Override
+    public Car create(Car car) {
+        return carDao.create(car);
+    }
+
+    @Override
+    public Car get(Long id) {
+        return carDao.get(id).get();
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return carDao.getAll();
+    }
+
+    @Override
+    public Car update(Car car) {
+        return carDao.update(car);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return carDao.delete(id);
+    }
+
+    @Override
+    public void setDriverToCar(Driver driver, Car car) {
+        car.getDrivers().add(driver);
+        update(car);
+    }
+
+    @Override
+    public void removeDriver(Driver driver, Car car) {
+        car.getDrivers().remove(driver);
+        update(car);
+    }
+
+    @Override
+    public List<Car> getAllByDriver(long driverId) {
+        return carDao.getAllByDriver(driverId);
+    }
+}
