c58ff0d9f910622a9bfb943093f5de501e08f9dc
Tetiana Chupika <mtv1301@gmail.com>
2021-08-12 12:03:47 +0300

    some refactored for checkstyle plugin

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -15,43 +15,28 @@ import mate.jdbc.model.Manufacturer;
 public class Main {
     public static void main(String[] args) {
         ManufacturerDao manufacturerDao = new ManufacturerDaoImpl();
-        Manufacturer BmwManufactured = new Manufacturer();
-        BmwManufactured.setCountry("Germany");
-        BmwManufactured.setName("BMWManufactured");
-        manufacturerDao.create(BmwManufactured);
+        Manufacturer bmwManufactured = new Manufacturer();
+        bmwManufactured.setCountry("Germany");
+        bmwManufactured.setName("BMWManufactured");
+        manufacturerDao.create(bmwManufactured);
 
-        Car BMW = new Car();
-        BMW.setModel("BMW");
-        BMW.setManufacturer(BmwManufactured);
-        CarDao newCar = new CarDaoImpl();
+        Car bmw = new Car();
+        bmw.setModel("bmw");
+        bmw.setManufacturer(bmwManufactured);
 
         DriverDao driverDao = new DriverDaoImpl();
 
-//        Driver Anton = new Driver();
-//        Anton.setName("Anton");
-//        Anton.setLicenseNumber("1919");
-//        driverDao.create(Anton);
-//
-//        Driver Misha = new Driver();
-//        Misha.setName("Misha");
-//        Misha.setLicenseNumber("2929");
-//        driverDao.create(Misha);
-//
-//        Driver Max = new Driver();
-//        Max.setName("Max");
-//        Max.setLicenseNumber("3939");
-//        driverDao.create(Max);
+        List<Driver> bmvDrivers = new ArrayList<>();
+        bmvDrivers.add(driverDao.get(1L).get());
+        bmvDrivers.add(driverDao.get(2L).get());
+        bmvDrivers.add(driverDao.get(3L).get());
+        bmw.setDrivers(bmvDrivers);
 
-        List<Driver> BMWDrivers = new ArrayList<>();
-        BMWDrivers.add(driverDao.get(1L).get());
-        BMWDrivers.add(driverDao.get(2L).get());
-        BMWDrivers.add(driverDao.get(3L).get());
-        BMW.setDrivers(BMWDrivers);
-
-        newCar.create(BMW);
-        System.out.println("bmw from DB before update:" + newCar.get(BMW.getId()));
-        BMW.getDrivers().remove(1);
-        newCar.update(BMW);
-        System.out.println("bmw from DB after update:" + newCar.get(BMW.getId()));
+        CarDao newCar = new CarDaoImpl();
+        newCar.create(bmw);
+        System.out.println("bmw from DB before update:" + newCar.get(bmw.getId()));
+        bmw.getDrivers().remove(1);
+        newCar.update(bmw);
+        System.out.println("bmw from DB after update:" + newCar.get(bmw.getId()));
     }
 }
