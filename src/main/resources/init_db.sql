e4d0a3e598a797ee836e09be08686c46130a3ec3
karaed <karaed.ml@mail.ru>
2021-09-13 14:56:34 +0300

    first_commit

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,5 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS `taxi` DEFAULT CHARACTER SET utf8;
-USE `taxi`;
+CREATE SCHEMA IF NOT EXISTS `taxi_db` DEFAULT CHARACTER SET utf8;
+USE `taxi_db`;
 
 DROP TABLE IF EXISTS `manufacturers`;
 CREATE TABLE `manufacturers` (
@@ -18,3 +18,21 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+CREATE TABLE `cars` (
+                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                        `manufacturer_id` BIGINT(11) NULL,
+                        `model` VARCHAR(255) NULL,
+                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                        PRIMARY KEY (`id`),
+                        CONSTRAINT `manufacturers_fk`
+                            FOREIGN KEY (`manufacturer_id`)
+                                REFERENCES `taxi_db`.`manufacturers` (`id`)
+                                ON DELETE NO ACTION
+                                ON UPDATE NO ACTION);
+
+CREATE TABLE `cars_drivers` (
+                                `car_id` BIGINT(11) NOT NULL,
+                                `driver_id` BIGINT(11) NOT NULL,
+                                CONSTRAINT `cars_fk` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`),
+                                CONSTRAINT `drivers_fk` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`));
