51e81fa2bb0ae860bd0004df58e08960bf0b97af
Roman Kozhevnikow <roman.kozhevnikow@gmail.com>
2021-10-04 18:03:58 +0300

    Added CarDao and CarService with all necessary methods.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,20 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
         // test your code here
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        carService.removeDriverFromCar(driverService.get(1L), carService.get(6L));
+        System.out.println(carService.get(6L));
     }
 }
