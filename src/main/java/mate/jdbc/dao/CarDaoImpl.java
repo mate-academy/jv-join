1dc7719f723e3335b917e2d68413b5f5f1222aa8
Andrii Demchenko <avd998ne@gmail.com>
2021-10-05 21:24:04 +0300

    changed connection settings, added getters and setters into Car model

--- src/main/java/mate/jdbc/dao/CarDaoImpl.java
+++ src/main/java/mate/jdbc/dao/CarDaoImpl.java
@@ -1,5 +1,38 @@
 package mate.jdbc.dao;
 
-public class CarDaoImpl {
+import mate.jdbc.model.Car;
 
+import java.util.List;
+
+public class CarDaoImpl implements CarDao {
+
+    @Override
+    public Car create(Car car) {
+        return null;
+    }
+
+    @Override
+    public Car get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return null;
+    }
+
+    @Override
+    public Car update(Car car) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public List<Car> getAllByDriver(Long driverId) {
+        return null;
+    }
 }
