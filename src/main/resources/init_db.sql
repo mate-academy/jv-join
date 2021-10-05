a8de827b4d67fa8ae4f70e2debd99b1c28212c68
Yehor Zapara <egor.zapara1994@gmail.com>
2021-10-05 16:20:42 +0300

    цршеуÑwhitespaces were deleted

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -43,5 +43,3 @@ CREATE TABLE `cars_drivers` (
                                 CONSTRAINT `cars_drivers_drivers_fk`
                                     FOREIGN KEY (`driver_id`)
                                     REFERENCES `drivers` (`id`));
-
-
