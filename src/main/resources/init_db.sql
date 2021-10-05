e350b740a37b2ba4e23da11512b6e0253ed8f114
Kyrylo Dvornyk <kiril75341@gmail.com>
2021-10-05 18:31:31 +0300

    Implemented solution for java-join task.

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,20 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+CREATE TABLE `cars` (
+                        `id` bigint NOT NULL AUTO_INCREMENT,
+                        `model` varchar(45) DEFAULT NULL,
+                        `is_deleted` varchar(45) DEFAULT '0',
+                        `manufacturer_id` bigint DEFAULT NULL,
+                        PRIMARY KEY (`id`),
+                        KEY `manufacturer_id_idx` (`manufacturer_id`),
+                        CONSTRAINT `cars_manufacturer_id` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `cars_drivers` (
+                                `car_id` bigint DEFAULT NULL,
+                                `driver_id` bigint DEFAULT NULL,
+                                KEY `cars_drivers_cars_id_idx` (`car_id`),
+                                KEY `cars_drivers_drivers_id_idx` (`driver_id`),
+                                CONSTRAINT `cars_drivers_cars_id` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`),
+                                CONSTRAINT `cars_drivers_drivers_id` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
