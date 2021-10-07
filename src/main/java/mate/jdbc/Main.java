f54a9c7157478bfba5cc93c37d557d4d96325038
V <v.ivashchenko42@protonmail.com>
2021-10-07 11:11:09 +0300

    created a link between tables. Created a Car object, created Dao and Service for it

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,31 @@
 package mate.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    public static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService) injector
+                .getInstance(ManufacturerService.class);
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Manufacturer manufacturer = new Manufacturer("BMW", "German");
+        manufacturer = manufacturerService.create(manufacturer);
+        Driver driver = new Driver("Vito", "32532");
+        driver = driverService.create(driver);
+        List<Driver> driversList = new ArrayList<>();
+        driversList.add(driver);
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        Car car = new Car("Q8", manufacturer);
+        car.setDrivers(driversList);
+        carService.create(car);
     }
 }
