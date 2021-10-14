7a31f02c79a8a58acda294e683be9d0ae9f97275
AlexPravda1 <kabinkos@gmail.com>
2021-10-14 23:00:02 +0400

    - checked ConnectionUtil

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // test your code here
         // test new laptop
+        int a = 5;
+        System.out.println(a);
     }
 }
