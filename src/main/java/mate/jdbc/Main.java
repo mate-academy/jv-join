c0a9876c576834a940deb6596c8a4807564ab728
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-10-15 22:25:07 +0300

    mvn fix

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,6 +1,5 @@
 package mate.jdbc;
 
-import java.util.ArrayList;
 import java.util.List;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Car;
@@ -16,23 +15,32 @@ public class Main {
     public static void main(String[] args) {
         ManufacturerService manufacturerService = (ManufacturerService)
                 injector.getInstance(ManufacturerService.class);
-        Manufacturer teslaManufacturer = manufacturerService.get(1L);
-        //manufacturerService.create(teslaManufacturer);
+        Manufacturer teslaManufacturer = new Manufacturer("Tesla","USA");
+        Manufacturer jeepManufacturer = new Manufacturer("Jeep","USA");
+        Manufacturer bentleyManufacturer = new Manufacturer("Bentley","England");
+        manufacturerService.create(teslaManufacturer);
+        manufacturerService.create(jeepManufacturer);
+        manufacturerService.create(bentleyManufacturer);
         System.out.println(manufacturerService.getAll());
 
-        DriverService driverService = (DriverService)
-                injector.getInstance(DriverService.class);
-        //Driver firstDriver = driverService.create(new Driver("test", "123123"));
-        //driverService.create(firstDriver);
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver firstDriver = driverService.create(new Driver("John", "675782777"));
+        Driver secondDriver = driverService.create(new Driver("Elza", "23553457"));
+        Driver thirdDriver = driverService.create(new Driver("Kit", "843336"));
+        driverService.create(firstDriver);
+        driverService.create(secondDriver);
+        driverService.create(thirdDriver);
         System.out.println(driverService.getAll());
 
-        List<Driver> drivers = new ArrayList<>();
-        //drivers.add(driverService.get(1l));
         CarService carService = (CarService) injector.getInstance(CarService.class);
-        Car firstCar = new Car("Model X", teslaManufacturer, drivers);
-        //carService.create(firstCar);
-
+        Car firstCar = new Car("Model X", teslaManufacturer, (List<Driver>) secondDriver);
+        Car secondCar = new Car("Model S", teslaManufacturer, (List<Driver>) thirdDriver);
+        Car thirdCar = new Car("Сontinental GT", bentleyManufacturer, (List<Driver>) firstDriver);
+        Car fourthCar = new Car("Cherokee", jeepManufacturer, (List<Driver>) firstDriver);
+        carService.create(firstCar);
+        carService.create(secondCar);
+        carService.create(thirdCar);
+        carService.create(fourthCar);
         System.out.println(carService.getAll());
-
     }
 }
