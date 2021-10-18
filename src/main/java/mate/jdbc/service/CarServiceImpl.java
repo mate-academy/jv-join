28c74c87ee333d2d7edbb199c4f0f5a60a7a5417
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-10-18 12:12:46 +0300

    mvn fix

--- src/main/java/mate/jdbc/service/CarServiceImpl.java
+++ src/main/java/mate/jdbc/service/CarServiceImpl.java
@@ -2,9 +2,7 @@ package mate.jdbc.service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-
 import mate.jdbc.dao.CarDao;
-import mate.jdbc.exception.DataProcessingException;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
 import mate.jdbc.model.Car;
