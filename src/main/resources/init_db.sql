7bbe3a88bae54732a3e4edb3cdcc0e03a7472782
Konstantin <www.kosta.kon@gmail.com>
2021-11-30 22:00:20 +0200

    CarService was implemented.

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,28 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars` (
+                           `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                           `model` VARCHAR(225) NOT NULL,
+                           `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                           `manufacturer_id` BIGINT(11) NULL,
+                           PRIMARY KEY (`id`),
+                           CONSTRAINT `taxi_manufacturer_fk`
+                                FOREIGN KEY (`manufacturer_id`)
+                                    REFERENCES `taxi`.`manufacturers` (`id`)
+                       );
+
+DROP TABLE IF EXISTS `cars_drivers`;
+CREATE TABLE `cars_drivers` (
+                        `car_id` BIGINT(11) NOT NULL,
+                        `driver_id` BIGINT(11) NOT NULL,
+                        CONSTRAINT `taxi_driver_fk`
+                            FOREIGN KEY (`driver_id`)
+                                REFERENCES `taxi`.`drivers` (`id`),
+                        CONSTRAINT `taxi_cars_fk`
+                            FOREIGN KEY (`car_id`)
+                                REFERENCES `taxi`.`cars` (`id`)
+);
+
