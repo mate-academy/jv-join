61fb5a35e08891d613b1e3226b44f6d3888c5c6e
Roman Polulikh <polilikh.r@gmail.com>
2021-12-03 14:27:23 +0200

    removed unused import

--- src/main/java/mate/jdbc/service/CarServiceImpl.java
+++ src/main/java/mate/jdbc/service/CarServiceImpl.java
@@ -2,7 +2,6 @@ package mate.jdbc.service;
 
 import java.util.List;
 import mate.jdbc.dao.CarDao;
-import mate.jdbc.exception.DataProcessingException;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
 import mate.jdbc.model.Car;
