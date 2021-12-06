cae5628211aad8ac4e3e11a3647ae60c70b56dbd
Volodimir <vova.stepanenko2020@gmail.com>
2021-12-06 09:45:05 +0200

    init sql del spaces

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -21,18 +21,16 @@ CREATE TABLE `drivers` (
 
 DROP TABLE IF EXISTS `cars`;
 CREATE TABLE `cars` (
-                        `id` bigint NOT NULL AUTO_INCREMENT,
-                        `model` varchar(255) DEFAULT NULL,
-                        `is_deleted` tinyint NOT NULL DEFAULT 0,
-                        `manufacturer_id` bigint DEFAULT NULL,
+                        `id` BIGINT NOT NULL AUTO_INCREMENT,
+                        `model` VARCHAR (255) DEFAULT NULL,
+                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                        `manufacturer_id` BIGINT DEFAULT NULL,
                         PRIMARY KEY (`id`),
-
                         CONSTRAINT `cars_manufacturers_fk` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`));
 
 DROP TABLE IF EXISTS `cars_drivers`;
 CREATE TABLE `cars_drivers` (
-                                `car_id` bigint NOT NULL,
-                                `driver_id` bigint NOT NULL,
-
+                                `car_id` BIGINT NOT NULL,
+                                `driver_id` BIGINT NOT NULL,
                                 CONSTRAINT `cars_drivers_cars_fk` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`),
                                 CONSTRAINT `cars_drivers_drivers_fk` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`));
