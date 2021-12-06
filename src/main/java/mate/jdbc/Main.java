1de93a2e5f11561d762c3f9c16ad8a9e32689ab7
Volodimir <vova.stepanenko2020@gmail.com>
2021-12-06 09:41:41 +0200

    Join hw taxi

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,47 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        Manufacturer zaz = new Manufacturer("Zaz", "Ukraine");
+        manufacturerService.create(zaz);
+
+        Car lanos = new Car("lanos", zaz);
+        Car sens = new Car("sens", zaz);
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        carService.create(lanos);
+        carService.create(sens);
+
+        Driver vasil = new Driver("Vasil", "fs5ss8");
+        Driver yuriy = new Driver("Yuriy", "s6s5d4");
+        Driver anna = new Driver("Anna", "asd5s1");
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        driverService.create(yuriy);
+        driverService.create(vasil);
+        driverService.create(anna);
+
+        carService.addDriverToCar(vasil, sens);
+        carService.addDriverToCar(vasil, lanos);
+        carService.addDriverToCar(anna, sens);
+        carService.addDriverToCar(anna, lanos);
+        carService.addDriverToCar(yuriy, sens);
+
+        carService.getAllByDriver(vasil.getId()).forEach(System.out::println);
+        System.out.println("**********");
+        carService.removeDriverFromCar(vasil, sens);
+
+        carService.getAllByDriver(vasil.getId()).forEach(System.out::println);
+
     }
 }
