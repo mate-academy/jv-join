fdd0a6d59a9e7796f37d39370fb04bb1a4d2704c
Eugene-exe <zhekacauselove@gmail.com>
2021-12-06 19:33:12 +0200

    My solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,114 @@
 package mate.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerDao = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+
+        Manufacturer ferrari = new Manufacturer();
+        ferrari.setCountry("Italy");
+        ferrari.setName("Ferrari");
+        manufacturerDao.create(ferrari);
+
+        Manufacturer porsche = new Manufacturer();
+        porsche.setCountry("Germany");
+        porsche.setName("Porsche");
+        manufacturerDao.create(porsche);
+
+        Manufacturer koenigsegg = new Manufacturer();
+        koenigsegg.setCountry("Sweden");
+        koenigsegg.setName("Koenigsegg");
+        manufacturerDao.create(koenigsegg);
+
+        System.out.println(manufacturerDao.get(3L));
+        manufacturerDao.getAll().forEach(System.out::println);
+
+        porsche.setCountry("Ukraine");
+        manufacturerDao.update(porsche);
+
+        manufacturerDao.getAll().forEach(System.out::println);
+
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+
+        Driver eugene = new Driver();
+        eugene.setName("Eugene");
+        eugene.setLicenseNumber("888");
+        driverService.create(eugene);
+
+        Driver andrew = new Driver();
+        andrew.setName("Andrew");
+        andrew.setLicenseNumber("777");
+        driverService.create(andrew);
+
+        Driver daniel = new Driver();
+        daniel.setName("Daniel");
+        daniel.setLicenseNumber("666");
+        driverService.create(daniel);
+
+        System.out.println(driverService.get(2L));
+        driverService.getAll().forEach(System.out::println);
+
+        daniel.setLicenseNumber("88");
+        driverService.update(daniel);
+
+        driverService.getAll().forEach(System.out::println);
+
+        List<Driver> ferrariDrivers = new ArrayList<>();
+        ferrariDrivers.add(daniel);
+        ferrariDrivers.add(andrew);
+
+        List<Driver> koenigseggDrivers = new ArrayList<>();
+        koenigseggDrivers.add(eugene);
+        koenigseggDrivers.add(andrew);
+        koenigseggDrivers.add(daniel);
+
+        List<Driver> porscheDrivers = new ArrayList<>();
+        porscheDrivers.add(eugene);
+
+        Car ferrariCar = new Car();
+        ferrariCar.setManufacturer(ferrari);
+        ferrariCar.setModel("Roma");
+        ferrariCar.setDrivers(ferrariDrivers);
+
+        Car koenigseggCar = new Car();
+        koenigseggCar.setManufacturer(koenigsegg);
+        koenigseggCar.setModel("Agera R");
+        koenigseggCar.setDrivers(koenigseggDrivers);
+
+        Car porscheCar = new Car();
+        porscheCar.setManufacturer(porsche);
+        porscheCar.setModel("911 turbo s");
+        porscheCar.setDrivers(porscheDrivers);
+
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        carService.create(ferrariCar);
+        carService.create(koenigseggCar);
+        carService.create(porscheCar);
+
+        carService.getAll().forEach(System.out::println);
+
+        ferrariCar.setModel("488 Pista");
+        carService.update(ferrariCar);
+
+        carService.delete(ferrariCar.getId());
+        carService.removeDriverFromCar(daniel, ferrariCar);
+        carService.addDriverToCar(eugene, ferrariCar);
+        carService.update(ferrariCar);
+
+        carService.getAllByDriver(andrew.getId());
+
+        carService.getAll().forEach(System.out::println);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,20 +1,37 @@
-CREATE SCHEMA IF NOT EXISTS `taxi` DEFAULT CHARACTER SET utf8;
-USE `taxi`;
+CREATE DATABASE `taxi_service_db` DEFAULT CHARACTER SET utf8;
+USE taxi_service_db ;
 
-DROP TABLE IF EXISTS `manufacturers`;
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+                                 `id` bigint NOT NULL AUTO_INCREMENT,
+                                 `name` varchar(255) NOT NULL,
+                                 `country` varchar(255) NOT NULL,
+                                 `is_deleted` tinyint DEFAULT '0',
+                                 PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `drivers`;
 CREATE TABLE `drivers` (
-                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                  `name` VARCHAR(225) NOT NULL,
-                                  `license_number` VARCHAR(225) NOT NULL,
-                                  `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                  PRIMARY KEY (`id`),
-                                  UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
-                                  UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `name` varchar(255) NOT NULL,
+                           `licenseNumber` varchar(255) NOT NULL,
+                           `is_deleted` tinyint NOT NULL DEFAULT '0',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `cars` (
+                        `id` bigint NOT NULL AUTO_INCREMENT,
+                        `model` varchar(255) NOT NULL,
+                        `manufacturer_id` bigint NOT NULL,
+                        `is_deleted` tinyint NOT NULL DEFAULT '0',
+                        PRIMARY KEY (`id`),
+                        KEY `table_name_manufacture_fk` (`manufacturer_id`),
+                        CONSTRAINT `table_name_manufacture_fk` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `cars_drivers` (
+                                `driver_id` bigint NOT NULL,
+                                `car_id` bigint NOT NULL,
+                                KEY `cars_drivers_cars_fk` (`car_id`),
+                                KEY `cars_drivers_drivers_fk` (`driver_id`),
+                                CONSTRAINT `cars_drivers_cars_fk` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`),
+                                CONSTRAINT `cars_drivers_drivers_fk` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
