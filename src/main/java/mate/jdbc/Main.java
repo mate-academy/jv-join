210f9eeef75ec52fb9db186be7988a551400fb41
Artik <Artik25942@gmail.com>
2021-12-07 14:19:56 +0200

    after mr

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -16,32 +16,38 @@ public class Main {
     public static void main(String[] args) {
         ManufacturerService manufacturerService =
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
-        manufacturerService.create(new Manufacturer("Audi", "Germany"));
-        manufacturerService.create(new Manufacturer("Lada", "Russia"));
-        Manufacturer manufacturerTest = new Manufacturer("Zaz", "Ukraine");
-        manufacturerService.create(manufacturerTest);
+        Manufacturer manufacturer1 = new Manufacturer("Audi", "Germany");
+        Manufacturer manufacturer2 = new Manufacturer("Lada", "Russia");
+        Manufacturer manufacturer3 = new Manufacturer("Zaz", "Ukraine");
+
+        manufacturerService.create(manufacturer1);
+        manufacturerService.create(manufacturer2);
+        manufacturerService.create(manufacturer3);
 
         DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
-        driverService.create(new Driver("Ihor", "123"));
-        driverService.create(new Driver("Bohdan", "234"));
-        Driver driverTest = new Driver("Volodymyr", "345");
-        driverService.create(driverTest);
+        Driver driver1 = new Driver("Ihor", "123");
+        Driver driver2 = new Driver("Bohdan", "234");
+        Driver driver3 = new Driver("Volodymyr", "345");
+
+        driverService.create(driver1);
+        driverService.create(driver2);
+        driverService.create(driver3);
 
         List<Driver> driversTest = new ArrayList<>();
-        driversTest.add(driverTest);
+        driversTest.add(driver3);
         CarService carService = (CarService) injector.getInstance(CarService.class);
-        Car carTest = new Car("Lanos", manufacturerTest, driversTest);
+        Car carTest = new Car("Lanos", manufacturer3, driversTest);
         System.out.println(carService.create(carTest) + " created");
-        System.out.println("Get " + carService.get(16L));
+        System.out.println("Get " + carService.get(carTest.getId()));
         System.out.println("Get all " + carService.getAll());
 
-        carService.addDriverToCar(driverService.get(1L), carTest);
+        carService.addDriverToCar(driverService.get(driver1.getId()), carTest);
         System.out.println(carService.getAll());
 
-        System.out.println("GetAllByDriver " + carService.getAllByDriver(3L));
+        System.out.println("GetAllByDriver " + carService.getAllByDriver(driver3.getId()));
 
-        System.out.println(carService.delete(16L) + " deleted");
+        System.out.println(carService.delete(carTest.getId()) + " deleted");
         System.out.println(carService.getAll());
-        System.out.println("GetAllByDriver " + carService.getAllByDriver(3L));
+        System.out.println("GetAllByDriver " + carService.getAllByDriver(driver3.getId()));
     }
 }
