29135c1417c45cb4c2e47c858e20baa2483d5b0f
Volodimir <vova.stepanenko2020@gmail.com>
2021-12-07 10:54:50 +0200

    add some test case;

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,5 +1,6 @@
 package mate.jdbc;
 
+import java.sql.SQLOutput;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Car;
 import mate.jdbc.model.Driver;
@@ -17,12 +18,6 @@ public class Main {
         Manufacturer zaz = new Manufacturer("Zaz", "Ukraine");
         manufacturerService.create(zaz);
 
-        Car lanos = new Car("lanos", zaz);
-        Car sens = new Car("sens", zaz);
-        CarService carService = (CarService) injector.getInstance(CarService.class);
-        carService.create(lanos);
-        carService.create(sens);
-
         Driver vasil = new Driver("Vasil", "fs5ss8");
         Driver yuriy = new Driver("Yuriy", "s6s5d4");
         Driver anna = new Driver("Anna", "asd5s1");
@@ -31,17 +26,37 @@ public class Main {
         driverService.create(vasil);
         driverService.create(anna);
 
+        Car lanos = new Car("lanos", zaz);
+        Car sens = new Car("sens", zaz);
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        carService.create(lanos);
+        carService.create(sens);
         carService.addDriverToCar(vasil, sens);
         carService.addDriverToCar(vasil, lanos);
         carService.addDriverToCar(anna, sens);
         carService.addDriverToCar(anna, lanos);
         carService.addDriverToCar(yuriy, sens);
 
+        System.out.println("**********");
+        System.out.println("Change model lanos");
+        System.out.println(carService.get(lanos.getId()));
+        lanos.setModel("lanos sx");
+        carService.update(lanos);
+        System.out.println(carService.get(lanos.getId()));
+        System.out.println("**********");
+        System.out.println("Get all cars");
+        System.out.println(carService.getAll());
+        System.out.println("**********");
+        System.out.println("Print all drivers after add");
         carService.getAllByDriver(vasil.getId()).forEach(System.out::println);
         System.out.println("**********");
+        System.out.println("Remove some drivers");
         carService.removeDriverFromCar(vasil, sens);
-
         carService.getAllByDriver(vasil.getId()).forEach(System.out::println);
+
+
+
+
 
     }
 }
