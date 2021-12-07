fc91355f7430efea7f12ea6d4e2d926f5d75c878
Admin Oleksandr <maslo5@meta.ua>
2021-12-07 10:38:16 +0200

    fixed init_db.sql file

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,4 +1,7 @@
-CREATE DATABASE `taxi_service_db`  DEFAULT CHARACTER SET utf8   DEFAULT ENCRYPTION='N';
+CREATE SCHEMA IF NOT EXISTS `taxi_service_db`  DEFAULT CHARACTER SET utf8   DEFAULT ENCRYPTION='N';
+USE `taxi_service_db`;
+
+DROP TABLE IF EXISTS `manufacturers`;
 CREATE TABLE `manufacturers` (
                                  `id` bigint NOT NULL AUTO_INCREMENT,
                                  `name` varchar(255) DEFAULT NULL,
@@ -6,6 +9,8 @@ CREATE TABLE `manufacturers` (
                                  `is_deleted` tinyint NOT NULL DEFAULT '0',
                                  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `drivers`;
 CREATE TABLE `drivers` (
                            `id` bigint NOT NULL AUTO_INCREMENT,
                            `name` varchar(255) DEFAULT NULL,
@@ -13,17 +18,24 @@ CREATE TABLE `drivers` (
                            `is_deleted` tinyint NOT NULL DEFAULT '0',
                            PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `cars`;
 CREATE TABLE `cars` (
                         `id` bigint NOT NULL AUTO_INCREMENT,
                         `model` varchar(255) DEFAULT 'null',
                         `is_deleted` tinyint NOT NULL DEFAULT '0',
                         `manufacturer_id` bigint DEFAULT NULL,
                         PRIMARY KEY (`id`),
+                        KEY `cars_manufacturer_fk` (`manufacturer_id`),
                         CONSTRAINT `cars_manufacturer_fk` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `cars_drivers`;
 CREATE TABLE `cars_drivers` (
                                 `car_id` bigint NOT NULL,
                                 `driver_id` bigint NOT NULL,
+                                KEY `cars_fk` (`car_id`),
+                                KEY `drivers_fk` (`driver_id`),
                                 CONSTRAINT `cars_fk` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`),
                                 CONSTRAINT `drivers_fk` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
