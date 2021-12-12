42f67ef2828a1d752dc78ee03d58cf3e0424d163
ASelivonchyk <aselivonchyk@gmail.com>
2021-12-12 20:42:59 +0200

    Wrote an assignment jv-join. - created class Car in package model; - created CarDao with CRUD methods and implement it in class CarDaoImpl; - created CarService and implement it in class CarServiceImpl; - added to init_db.sql parameters to create "cars" and "car_drivers" tables in DB; in Main class call all of CarsService methods; - moved all implementations to "impl" package.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,68 @@
 package mate.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        Car firstCar = new Car();
+        firstCar.setModel("polo");
+        Car secondCar = new Car();
+        secondCar.setModel("rapid");
+        Car thirdCar = new Car();
+        thirdCar.setModel("accent");
+
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        Manufacturer volkswagen = manufacturerService
+                .create(new Manufacturer("Volkswagen", "Germany"));
+        firstCar.setManufacturer(volkswagen);
+        Manufacturer skoda = manufacturerService
+                .create(new Manufacturer("Skoda", "Czech Republic"));
+        secondCar.setManufacturer(skoda);
+        Manufacturer hyundai = manufacturerService
+                .create(new Manufacturer("Hyundai", "Republic of Korea"));
+        thirdCar.setManufacturer(hyundai);
+
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver aleks = driverService.create(new Driver("Aleks", "DK012345D"));
+        Driver sergey = driverService.create(new Driver("Sergey", "AA54321D"));
+        List<Driver> firstDriversGroup = new ArrayList<>();
+        firstDriversGroup.add(aleks);
+        firstDriversGroup.add(sergey);
+        firstCar.setDrivers(firstDriversGroup);
+
+        Driver boris = driverService.create(new Driver("Boris", "BS56789B"));
+        Driver roman = driverService.create(new Driver("Roman", "CR09874H"));
+        List<Driver> secondDriversGroup = new ArrayList<>();
+        secondDriversGroup.add(boris);
+        secondDriversGroup.add(roman);
+        secondCar.setDrivers(secondDriversGroup);
+
+        List<Driver> thirdDriversGroup = new ArrayList<>();
+        thirdDriversGroup.add(aleks);
+        thirdCar.setDrivers(thirdDriversGroup);
+
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        carService.create(firstCar);
+        carService.create(secondCar);
+        carService.create(thirdCar);
+        carService.getAll().forEach(System.out::println);
+        carService.getAllByDriver(1L).forEach(System.out::println);
+        thirdCar.setModel("sonata");
+        carService.update(thirdCar);
+        carService.delete(2L);
+        carService.addDriverToCar(roman, thirdCar);
+        carService.removeDriverFromCar(aleks, thirdCar);
+        System.out.println(carService.get(3L));
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,7 +1,8 @@
-CREATE SCHEMA IF NOT EXISTS `taxi` DEFAULT CHARACTER SET utf8;
-USE `taxi`;
+CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
+USE `taxi_service`;
 
-DROP TABLE IF EXISTS `manufacturers`;
+DROP TABLE IF EXISTS `car_drivers`, `cars`, `drivers`, `manufacturers`;
+
 CREATE TABLE `manufacturers` (
                                         `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
                                         `name` VARCHAR(225) NOT NULL,
@@ -9,7 +10,6 @@ CREATE TABLE `manufacturers` (
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
 
-DROP TABLE IF EXISTS `drivers`;
 CREATE TABLE `drivers` (
                                   `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
                                   `name` VARCHAR(225) NOT NULL,
@@ -18,3 +18,29 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+CREATE TABLE `cars` (
+                        `id` bigint NOT NULL AUTO_INCREMENT,
+                        `model` varchar(225) NOT NULL,
+                        `manufacturer_id` bigint NOT NULL,
+                        `is_deleted` tinyint NOT NULL DEFAULT 0,
+                        PRIMARY KEY (`id`),
+                        KEY `cars_manufacturers_fk` (`manufacturer_id`),
+                        CONSTRAINT `cars_manufacturers_fk` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`)
+                        ON DELETE NO ACTION
+                        ON UPDATE NO ACTION);
+
+CREATE TABLE `car_drivers` (
+                               `car_id` bigint NOT NULL,
+                               `driver_id` bigint NOT NULL,
+                               KEY `car_drivers_cars_fk` (`car_id`),
+                               KEY `car_drivers_drivers_fk` (`driver_id`),
+                               CONSTRAINT `car_drivers_cars_fk` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`)
+                               ON DELETE NO ACTION
+                               ON UPDATE NO ACTION,
+                               CONSTRAINT `car_drivers_drivers_fk` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`)
+                               ON DELETE NO ACTION
+                               ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
