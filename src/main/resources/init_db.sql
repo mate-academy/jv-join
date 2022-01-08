7600bee0b61f64dadf77d0a2cb7b83f56e6e7ee7
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-01-08 20:20:34 +0200

    Updated init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,31 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+CREATE TABLE `taxi`.`cars` (
+  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `model` VARCHAR(255) NULL,
+  `is_deleted` TINYINT NULL DEFAULT 0,
+  `manufacturer_id` BIGINT(11) NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `manufacturer_id_fk`
+								FOREIGN KEY (`manufacturer_id`)
+									REFERENCES `taxi`.`manufacturers` (`id`)
+                                    ON UPDATE NO ACTION
+                                    ON DELETE NO ACTION
+  );
+
+CREATE TABLE `taxi`.`cars_drivers` (
+  `driver_id` BIGINT(11) NOT NULL,
+  `car_id` BIGINT(11) NOT NULL,
+  CONSTRAINT `driver_id_fk`
+								FOREIGN KEY (`driver_id`)
+									REFERENCES `taxi`.`drivers` (`id`)
+                                    ON UPDATE NO ACTION
+                                    ON DELETE NO ACTION,
+  CONSTRAINT `car_id_fk`
+								FOREIGN KEY (`car_id`)
+									REFERENCES `taxi`.`cars` (`id`)
+                                    ON UPDATE NO ACTION
+                                    ON DELETE NO ACTION
+  );
