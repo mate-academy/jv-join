de269fac3af341c412ee786da44e07bb8fdeb076
sviat <svyatoslav.grinik@gmail.com>
2022-02-15 13:01:03 +0200

    added DAO layer for car model

---
+++ src/main/java/mate/jdbc/dao/CarDaoImpl.java
@@ -0,0 +1,38 @@
+package mate.jdbc.dao;
+
+import java.util.List;
+import mate.jdbc.lib.Dao;
+import mate.jdbc.model.Car;
+
+@Dao
+public class CarDaoImpl implements CarDao {
+    @Override
+    public Car create(Car car) {
+        return null;
+    }
+
+    @Override
+    public Car get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return null;
+    }
+
+    @Override
+    public Car update(Car car) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public List<Car> getAllByDriver(Long driverId) {
+        return null;
+    }
+}
