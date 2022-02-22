3b73d0f7232acc6e6635b2324191f9b70faf850a
sviat <svyatoslav.grinik@gmail.com>
2022-02-22 22:21:19 +0200

    fixed checkstyle issues

--- src/main/java/mate/jdbc/dao/CarDaoImpl.java
+++ src/main/java/mate/jdbc/dao/CarDaoImpl.java
@@ -1,13 +1,13 @@
 package mate.jdbc.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import mate.jdbc.exception.DataProcessingException;
 import mate.jdbc.lib.Dao;
 import mate.jdbc.model.Car;
