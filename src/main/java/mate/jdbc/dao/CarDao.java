454dae133d3b37a4e654c5837378ed3e7f98b2fd
LinkClink <shekaqwe@gmail.com>
2022-03-08 14:57:42 +0200

    optional fix

--- src/main/java/mate/jdbc/dao/CarDao.java
+++ src/main/java/mate/jdbc/dao/CarDao.java
@@ -1,13 +1,14 @@
 package mate.jdbc.dao;
 
 import java.util.List;
+import java.util.Optional;
 import mate.jdbc.model.Car;
 
 public interface CarDao {
 
     Car create(Car car);
 
-    Car get(Long id);
+    Optional<Car> get(Long id);
 
     List<Car> getAll();
 
