4e5e450e48a7ef7d1ee33ce9a8174984f1a18bca
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-03-10 17:40:27 +0200

    Structure of project was crated

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,5 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
+CREATE SCHEMA IF NOT EXISTS `taxi_service_db` DEFAULT CHARACTER SET utf8;
+USE `taxi_service_db`;
 
 DROP TABLE IF EXISTS `manufacturers`;
 CREATE TABLE `manufacturers` (
@@ -18,3 +18,27 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars` (
+                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                  `manufacturer_id` BIGINT(11) NULL,
+                                  `model` VARCHAR(225) NOT NULL,
+                                  `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                  PRIMARY KEY (`id`),
+                                  CONSTRAINT `car_manufacturer_fk`
+                                    FOREIGN KEY (`manufacturer_id`)
+                                        REFERENCES `taxi_service_db`.`manufacturers` (`id`)
+                                        ON DELETE NO ACTION
+                                        ON UPDATE NO ACTION);
+
+DROP TABLE IF EXISTS `cars_drivers`;
+CREATE TABLE `cars_drivers` (
+                        `driver_id` BIGINT(11) NOT NULL,
+                        `car_id` BIGINT(11) NOT NULL,
+                        CONSTRAINT `cars_drivers_drivers_fk`
+                            FOREIGN KEY (`driver_id`)
+                                REFERENCES `taxi_service_db`.`drivers` (`id`),
+                        CONSTRAINT `cars_drivers_cars_fk`
+                            FOREIGN KEY (`car_id`)
+                                REFERENCES `taxi_service_db`.`cars` (`id`));
