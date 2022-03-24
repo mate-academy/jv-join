c1f74eadff82cf07cfbf690774afc74335dc2146
AprioriKS <sounddummies@gmail.com>
2022-03-24 12:45:58 +0200

    add fix

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -1,9 +1,9 @@
 package mate.jdbc.util;
 
-    import java.sql.Connection;
-    import java.sql.DriverManager;
-    import java.sql.SQLException;
-    import java.util.Properties;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/taxi_service_mysqldb";
