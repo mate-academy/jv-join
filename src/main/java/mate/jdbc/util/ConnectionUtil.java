f9d3da008c0bd40cfd5bee9093656e1874d4fb31
AprioriKS <sounddummies@gmail.com>
2022-03-24 12:42:31 +0200

    add fix

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -1,9 +1,9 @@
 package mate.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.SQLException;
+    import java.util.Properties;
 
 public class ConnectionUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/taxi_service_mysqldb";
@@ -19,10 +19,14 @@ public class ConnectionUtil {
         }
     }
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() {
         Properties dbProperties = new Properties();
         dbProperties.setProperty("user", USERNAME);
         dbProperties.setProperty("password", PASSWORD);
-        return DriverManager.getConnection(URL, dbProperties);
+        try {
+            return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB ", e);
+        }
     }
 }
