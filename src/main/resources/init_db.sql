2030401378b7f1367d36f94422cc0c73e6015dde
vladsysa <vladsysa@mail.ru>
2022-03-25 12:20:37 +0100

    After changes 2

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -6,7 +6,7 @@ CREATE TABLE `manufacturers` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `is_deleted` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=129 DEFAULT CHARSET=utf8mb3;
+)
 
 CREATE TABLE `drivers` (
   `name` varchar(211) DEFAULT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE `drivers` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `is_deleted` tinyint DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb3;
+)
 
 CREATE TABLE `cars` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -24,7 +24,7 @@ CREATE TABLE `cars` (
   PRIMARY KEY (`id`),
   KEY `cars_manufacturers_fk` (`manufacturers_id`),
   CONSTRAINT `cars_manufacturers_fk` FOREIGN KEY (`manufacturers_id`) REFERENCES `manufacturers` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb3;
+)
 
 CREATE TABLE `cars_drivers` (
   `car_id` bigint DEFAULT NULL,
@@ -33,5 +33,4 @@ CREATE TABLE `cars_drivers` (
   KEY `cars_drivers_drivers_fk` (`driver_id`),
   CONSTRAINT `cars_drivers_cars_fk` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`),
   CONSTRAINT `cars_drivers_drivers_fk` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
+)
