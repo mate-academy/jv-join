85528e0f9a793456f8a6a986a2d413d9b5c3bdf8
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-04-24 22:59:24 +0200

    corrected data

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,23 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+CREATE TABLE `taxi_db`.`cars` (
+                                  `id` BIGINT NOT NULL AUTO_INCREMENT,
+                                  `model` VARCHAR(255) NOT NULL,
+                                  `manufacturer_id` BIGINT NULL,
+                                  `driver_id` BIGINT NULL,
+                                  `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                  PRIMARY KEY (`id`),
+                                  INDEX `car_manufacturer_fk` (`driver_id` ASC) VISIBLE,
+                                  INDEX `car_drivers_fk` (`manufacturer_id` ASC) VISIBLE,
+                                  CONSTRAINT `car_drivers_fk`
+                                      FOREIGN KEY (`driver_id`)
+                                          REFERENCES `taxi_db`.`drivers` (`id`)
+                                          ON DELETE NO ACTION
+                                          ON UPDATE NO ACTION,
+                                  CONSTRAINT `car_manufacturer_fk`
+                                      FOREIGN KEY (`manufacturer_id`)
+                                          REFERENCES `taxi_db`.`manufacturer` (`id`)
+                                          ON DELETE NO ACTION
+                                          ON UPDATE NO ACTION);
