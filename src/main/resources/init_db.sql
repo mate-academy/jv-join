1905a0525e2520f7c948ad602cb6270934b0d11c
Mykola Skrypka <mykolaskrypka@gmail.com>
2022-05-12 15:55:30 +0300

    created class Car; created CarService interface with implementations

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,22 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars` (
+  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `model` VARCHAR(255) NOT NULL,
+  `manufacturer_id` BIGINT(11) DEFAULT NULL,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `cars_manufakturers_fk` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`)
+);
+
+DROP TABLE IF EXISTS `cars_drivers`;
+CREATE TABLE `cars_drivers` (
+  `car_id` BIGINT(11) NOT NULL,
+  `driver_id` BIGINT(11) NOT NULL,
+  CONSTRAINT `cars_drivers_cars_fk` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`),
+  CONSTRAINT `cars_drivers_drivers_fk` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`)
+) ;
+
