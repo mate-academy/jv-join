ea9038c123f51b101f63f07ea44c4883f77d96a3
Iryna Hrynda <hryndairyna1@gmail.com>
2022-05-13 19:10:53 +0300

    -fixed mistakes

--- src/main/java/mate/jdbc/dao/CarDaoImpl.java
+++ src/main/java/mate/jdbc/dao/CarDaoImpl.java
@@ -102,7 +102,6 @@ public class CarDaoImpl implements CarDao {
     @Override
     public boolean delete(Long id) {
         String query = "UPDATE cars SET is_deleted = TRUE WHERE id = ?;";
-        removeCarDrivers(id);
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement statement
                         = connection.prepareStatement(query)) {
