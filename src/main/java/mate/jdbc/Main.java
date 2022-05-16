c2c5666db3eb9f42cbb5a986746e2021d439ffa9
Андрій Кирильчук <101740223+Gigawatt94@users.noreply.github.com>
2022-05-16 16:52:13 +0300

    work done. mvn build success

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,60 @@
 package mate.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService
+                = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        Manufacturer manufacturerChallenger = new Manufacturer("Challenger", "USA");
+        Manufacturer manufacturerMercedes = new Manufacturer("Mercedes", "Germany");
+        manufacturerService.create(manufacturerChallenger);
+        manufacturerService.create(manufacturerMercedes);
+
+        Driver driverAzamat = new Driver("Azamat", "PAR156834");
+        Driver driverVazgen = new Driver("Vazgen", "GLO145687");
+        Driver driverBarak = new Driver("Barak", "KOD258645");
+        Driver driverAhmed = new Driver("Ahmed", "DON357951");
+
+        DriverService driverService
+                = (DriverService) injector.getInstance(DriverService.class);
+        driverService.create(driverAzamat);
+        driverService.create(driverVazgen);
+        driverService.create(driverBarak);
+        driverService.create(driverAhmed);
+
+        List<Driver> driverListDodge = new ArrayList<>();
+        List<Driver> driversListBuick = new ArrayList<>();
+
+        driverListDodge.add(driverAzamat);
+        driverListDodge.add(driverBarak);
+        driversListBuick.add(driverVazgen);
+        driversListBuick.add(driverAhmed);
+
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        Car carModelDodge = new Car("Challenger", manufacturerChallenger, driverListDodge);
+        Car carModelBuick = new Car("Reatta", manufacturerMercedes, driversListBuick);
+        carService.create(carModelDodge);
+        carService.create(carModelBuick);
+
+        carService.getAllByDriver(carModelDodge.getId());
+        carModelDodge.setManufacturer(manufacturerService.get(manufacturerChallenger.getId()));
+        carService.update(carModelDodge);
+        carService.getAll().forEach(System.out::println);
+        carService.removeDriverFromCar(driverBarak, carModelDodge);
+        carService.getAllByDriver(driverAzamat.getId()).forEach(System.out::println);
+
+        carService.delete(carModelBuick.getId());
+
     }
 }
--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -19,7 +19,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     public Manufacturer create(Manufacturer manufacturer) {
         String query = "INSERT INTO manufacturers (name, country) "
                 + "VALUES (?, ?)";
-        try (Connection connection = ConnectionUtil.getConnection();
+        try (Connection connection = (Connection) ConnectionUtil.getConnection();
                 PreparedStatement statement
                         = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, manufacturer.getName());
@@ -39,8 +39,8 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     public Optional<Manufacturer> get(Long id) {
         String query = "SELECT * FROM manufacturers"
                 + " WHERE id = ? AND is_deleted = FALSE";
-        try (Connection connection = ConnectionUtil.getConnection();
-                 PreparedStatement statement = connection.prepareStatement(query)) {
+        try (Connection connection = (Connection) ConnectionUtil.getConnection();
+                PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setLong(1, id);
             ResultSet resultSet = statement.executeQuery();
             Manufacturer manufacturer = null;
@@ -56,8 +56,8 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public List<Manufacturer> getAll() {
         String query = "SELECT * FROM manufacturers WHERE is_deleted = FALSE";
-        try (Connection connection = ConnectionUtil.getConnection();
-                 PreparedStatement statement
+        try (Connection connection = (Connection) ConnectionUtil.getConnection();
+                PreparedStatement statement
                          = connection.prepareStatement(query)) {
             List<Manufacturer> manufacturers = new ArrayList<>();
             ResultSet resultSet = statement.executeQuery();
@@ -75,7 +75,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     public Manufacturer update(Manufacturer manufacturer) {
         String query = "UPDATE manufacturers SET name = ?, country = ?"
                 + " WHERE id = ? AND is_deleted = FALSE";
-        try (Connection connection = ConnectionUtil.getConnection();
+        try (Connection connection = (Connection) ConnectionUtil.getConnection();
                  PreparedStatement statement
                          = connection.prepareStatement(query)) {
             statement.setString(1, manufacturer.getName());
@@ -92,7 +92,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public boolean delete(Long id) {
         String query = "UPDATE manufacturers SET is_deleted = TRUE WHERE id = ?";
-        try (Connection connection = ConnectionUtil.getConnection();
+        try (Connection connection = (Connection) ConnectionUtil.getConnection();
                  PreparedStatement statement
                          = connection.prepareStatement(query)) {
             statement.setLong(1, id);
