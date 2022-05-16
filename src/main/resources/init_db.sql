3dd4be4625a1531f8b36e014bb59a86e1e6f8ca5
mordast7 <mordast7@gmail.com>
2022-05-16 13:02:18 +0300

    add init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,23 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+CREATE TABLE `cars` (
+                        `id` bigint NOT NULL AUTO_INCREMENT,
+                        `model` varchar(45) DEFAULT NULL,
+                        `id_manufacturer` bigint DEFAULT NULL,
+                        `is_deleted` tinyint NOT NULL DEFAULT '0',
+                        PRIMARY KEY (`id`),
+                        KEY `id_idx` (`id_manufacturer`),
+                        CONSTRAINT `id` FOREIGN KEY (`id_manufacturer`) REFERENCES `manufacturers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+)
+CREATE TABLE `cars_drivers` (
+                                `id_car_driver` bigint NOT NULL AUTO_INCREMENT,
+                                `id_car` bigint DEFAULT NULL,
+                                `id_driver` bigint DEFAULT NULL,
+                                PRIMARY KEY (`id_car_driver`),
+                                KEY `id_cars_idx` (`id_car`),
+                                KEY `id_idx` (`id_driver`),
+                                CONSTRAINT `id_car` FOREIGN KEY (`id_car`) REFERENCES `cars` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+                                CONSTRAINT `id_driver` FOREIGN KEY (`id_driver`) REFERENCES `drivers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+)
+
