a6006e97ff573f22aaaa227613806b5da759a595
Vova Klimyuk <klimyuk054@gmail.com>
2022-05-16 15:06:03 +0300

    v1

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,20 +1,41 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
 
-DROP TABLE IF EXISTS `manufacturers`;
+CREATE SCHEMA IF NOT EXISTS `taxi_service_schema` DEFAULT CHARACTER SET utf8;
+USE `taxi_service_schema`;
+
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+                                 `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                 `name` VARCHAR(225) NOT NULL,
+                                 `country` VARCHAR(225) NOT NULL,
+                                 `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                 PRIMARY KEY (`id`));
 
-DROP TABLE IF EXISTS `drivers`;
 CREATE TABLE `drivers` (
-                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                  `name` VARCHAR(225) NOT NULL,
-                                  `license_number` VARCHAR(225) NOT NULL,
-                                  `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                  PRIMARY KEY (`id`),
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `name` varchar(45) DEFAULT NULL,
+                           `license_number` varchar(45) DEFAULT NULL,
+                           `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+CREATE TABLE `cars` (
+                        `id` bigint NOT NULL AUTO_INCREMENT,
+                        `model` varchar(255) DEFAULT NULL,
+                        `is_deleted` tinyint NOT NULL DEFAULT '0',
+                        `manufacturer_id` bigint DEFAULT NULL,
+                        PRIMARY KEY (`id`),
+                        KEY `cars_manufacturer_id_fk` (`manufacturer_id`),
+    CONSTRAINT `cars_manufacturer_id_fk` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`)
+)
+
+CREATE TABLE `cars_drivers` (
+	`car_id` BIGINT NOT NULL,
+    `driver_id` BIGINT NOT NULL,
+    CONSTRAINT `cars_drivers_cars_fk`
+										FOREIGN KEY (`car_id`)
+                                        REFERENCES `taxi_service_schema`.`cars` (`id`),
+	CONSTRAINT `cars_drivers_drivers_fk`
+										FOREIGN KEY (`driver_id`)
+                                        REFERENCES `taxi_service_schema`.`drivers` (`id`)
+);
