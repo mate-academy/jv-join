cd52b602c31e63bc9e95b32d6fb9869da8939626
GrabenkoVitaliy <grabenkovitaliy7@gmail.com>
2022-05-16 16:28:49 +0300

    fixed

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -14,26 +14,28 @@ public class Main {
 
     public static void main(String[] args) {
         DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
-        driverService.create(new Driver("John", "52631"));
-        driverService.create(new Driver("Bill", "1654"));
-        driverService.create(new Driver("Den", "79546"));
+        Driver john = new Driver("John", "52631");
+        Driver bill = new Driver("Bill", "1654");
+        Driver den = new Driver("Den", "79546");
+        driverService.create(john);
+        driverService.create(bill);
+        driverService.create(den);
         ManufacturerService manufacturerService =
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
-        manufacturerService.create(new Manufacturer("bmw", "Germany"));
-        manufacturerService.create(new Manufacturer("ferrari", "Italy"));
+        Manufacturer bmwManu = new Manufacturer("bmw", "Germany");
+        Manufacturer ferrafiManu = new Manufacturer("ferrari", "Italy");
+        manufacturerService.create(bmwManu);
+        manufacturerService.create(ferrafiManu);
         CarService carService = (CarService) injector.getInstance(CarService.class);
-        Car audi = new Car("audi", manufacturerService.get(10L),
-                List.of(driverService.get(1L), driverService.get(7L)));
-        Car bmw = new Car("BMW", manufacturerService.get(15L),
-                List.of(driverService.get(9L), driverService.get(10L)));
-        Car ferrari = new Car("Ferrari", manufacturerService.get(16L),
-                List.of(driverService.get(9L), driverService.get(10L), driverService.get(1L)));
-        carService.create(audi);
+        Car bmw = new Car("BMW", manufacturerService.get(bmwManu.getId()),
+                List.of(driverService.get(bill.getId()), driverService.get(john.getId())));
+        Car ferrari = new Car("Ferrari", manufacturerService.get(ferrafiManu.getId()),
+                List.of(driverService.get(bill.getId()), driverService.get(john.getId())));
         carService.create(bmw);
         carService.create(ferrari);
         System.out.println(carService.get(bmw.getId()));
-        carService.addDriverToCar(driverService.get(1L), bmw);
-        carService.getAllByDriver(10L);
+        carService.addDriverToCar(den, ferrari);
+        System.out.println(carService.getAllByDriver(ferrari.getId()));
         carService.delete(ferrari.getId());
     }
 }
