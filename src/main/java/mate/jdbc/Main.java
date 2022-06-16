ade5237135ce2bdddaaecdf06bfbd692d92b7497
Mykola Dubich <98731880+Sm0kk@users.noreply.github.com>
2022-06-16 17:21:40 +0300

    Created class Car, interface CarService and implementation. Created new tables in BD.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,80 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setName("Nissan");
+        manufacturer.setCountry("Japan");
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        manufacturerService.create(manufacturer);
+
+        Driver driver = new Driver();
+        driver.setName("Jake");
+        driver.setLicenseNumber("AE4576478");
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        driverService.create(driver);
+
+        driver.setName("Lora");
+        driver.setLicenseNumber("AE8645789");
+        driverService.create(driver);
+
+        driver.setName("Alice");
+        driver.setLicenseNumber("754");
+        driverService.create(driver);
+
+        List<Driver> drivers = driverService.getAll();
+
+        Car car1 = new Car();
+        car1.setModel("Skyline");
+        car1.setManufacturer(manufacturer);
+        car1.setDrivers(drivers);
+        CarService carService =
+                (CarService) injector.getInstance(CarService.class);
+        carService.create(car1);
+
+        List<Car> all = carService.getAll();
+        all.forEach(System.out::println);
+
+        List<Car> allByDriver = carService.getAllByDriver(11L);
+        allByDriver.forEach(System.out::println);
+
+        System.out.println(carService.get(1L));
+
+        car1 = all.get(0);
+        drivers = car1.getDrivers();
+        drivers.get(0).setName("Joni D");
+        driverService.update(drivers.get(0));
+        carService.update(car1);
+
+        all = carService.getAll();
+        all.forEach(System.out::println);
+
+        Driver newDriver = new Driver();
+        newDriver.setName("Christopher L");
+        newDriver.setLicenseNumber("ER567MN");
+        driverService.create(newDriver);
+        carService.addDriverToCar(newDriver, car1);
+        all = carService.getAll();
+        all.forEach(System.out::println);
+
+        Driver removeDriver = all.get(0).getDrivers().get(1);
+        carService.removeDriverFromCar(removeDriver, car1);
+        all = carService.getAll();
+        all.forEach(System.out::println);
+        driverService.getAll().forEach(System.out::println);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,17 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+CREATE TABLE cars (
+                      `id` BIGINT NOT NULL AUTO_INCREMENT,
+                      `manufacturer_id` BIGINT NULL,
+                      `model` VARCHAR(255) NULL,
+                      `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                      PRIMARY KEY (`id`),
+
+                      CONSTRAINT `cars_manufacturers_fk`
+                          FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`));
+CREATE TABLE `cars_drivers` (
+                                `driver_id` BIGINT NOT NULL,
+                                `car_id` BIGINT NOT NULL,
+                                CONSTRAINT `cars_drivers_drivers_fk` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`),
+                                CONSTRAINT `cars_drivers_cars_fk` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`));
