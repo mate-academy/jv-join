7bc54572a5f98b3d841e523e7bdbf2fb58770b63
Alexander <aleksandr_pogreb@ukr.net>
2022-06-19 21:21:41 +0300

    jv-join was added

--- src/main/java/mate/jdbc/lib/Injector.java
+++ src/main/java/mate/jdbc/lib/Injector.java
@@ -11,7 +11,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Injector {
+public class
+        Injector {
     private static final Map<String, Injector> injectors = new HashMap<>();
     private final Map<Class<?>, Object> instanceOfClasses = new HashMap<>();
     private final List<Class<?>> classes = new ArrayList<>();
