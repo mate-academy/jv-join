2077cc6a47109c2c0d2dc9cac1293227e005a8c9
Andrey Chumak <chumakandrey7@gmail.com>
2022-06-25 15:39:19 +0300

    Created model Car Implemented CarDao and CarService

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,39 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        List<Driver> driverList = driverService.getAll();
+
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        Manufacturer manufacturer = manufacturerService.get(17L);
+
+        Car audi = new Car();
+        audi.setModel("A6");
+        audi.setDrivers(driverList);
+        audi.setManufacturer(manufacturer);
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        carService.create(audi);
+
+        System.out.println(carService.getAll());
+
+        manufacturer.setId(18L);
+        audi.setManufacturer(manufacturer);
+        carService.update(audi);
+        System.out.println(carService.get(audi.getId()));
+        System.out.println(carService.getAllByDriver(driverList.get(1).getId()));
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,29 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+CREATE TABLE taxi_service_db.cars (
+		`id` BIGINT NOT NULL AUTO_INCREMENT,
+        `model` VARCHAR(255) NULL,
+        `manufacturer_id` BIGINT NULL,
+        `is_deleted` TINYINT NOT NULL DEFAULT 0,
+        PRIMARY KEY (`id`),
+        CONSTRAINT `cars_manufacturer_fk`
+									FOREIGN KEY (`manufacturer_id`)
+                                    REFERENCES `taxi_service_db`.`manufacturers` (`id`)
+                                    ON DELETE NO ACTION
+									ON UPDATE NO ACTION);
+
+CREATE TABLE taxi_service_db.cars_drivers (
+		`car_id` BIGINT NOT NULL,
+		`driver_id` BIGINT NOT NULL,
+        CONSTRAINT `cars_drivers_cars_fk`
+									FOREIGN KEY (`car_id`)
+                                    REFERENCES `taxi_service_db`.`cars` (`id`)
+                                    ON DELETE NO ACTION
+									ON UPDATE NO ACTION,
+		CONSTRAINT `cars_drivers_drivers_fk`
+									FOREIGN KEY (`driver_id`)
+                                    REFERENCES `taxi_service_db`.`drivers` (`id`)
+                                    ON DELETE NO ACTION
+									ON UPDATE NO ACTION);
