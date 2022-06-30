ae0d6da4d7be605fbba81e1ec7ee1fe1cf618ea2
Misha Polunin <polunin1986@gmail.com>
2022-06-30 16:18:26 +0300

    -done

--- src/main/java/mate/jdbc/service/CarServiceImpl.java
+++ src/main/java/mate/jdbc/service/CarServiceImpl.java
@@ -21,8 +21,8 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public Car get(Long id) {
-        return carDao.get(id).orElseThrow(() -> new NoSuchElementException("There is no car with id "
-                + "= " + id));
+        return carDao.get(id).orElseThrow(() ->
+                new NoSuchElementException("There is no car with id " + "= " + id));
     }
 
     @Override
