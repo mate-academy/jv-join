c4b8b20304011e5ef4121c9c6d47f5714260d12e
Oleksii Kordysh <aleksey.kordysh@gmail.com>
2022-07-06 15:30:43 +0300

    Tests moved to Main, No hardcoded values, try/catch restored in ConnectionUtil

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -19,10 +19,14 @@ public class ConnectionUtil {
         }
     }
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() {
         Properties dbProperties = new Properties();
         dbProperties.setProperty("user", USERNAME);
         dbProperties.setProperty("password", PASSWORD);
-        return DriverManager.getConnection(URL, dbProperties);
+        try {
+            return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB ", e);
+        }
     }
 }
