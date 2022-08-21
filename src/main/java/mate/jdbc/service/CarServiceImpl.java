5e355892a761b754cf12841ece20de5e9e8eb3ba
Aleksandr Savchenko <savchenko.arts@gmail.com>
2022-08-21 23:58:38 +0300

    HW_JOIN

---
+++ src/main/java/mate/jdbc/service/CarServiceImpl.java
@@ -0,0 +1,48 @@
+package mate.jdbc.service;
+
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+
+public class CarServiceImpl implements CarService {
+    @Override
+    public Car create(Car car) {
+        return null;
+    }
+
+    @Override
+    public Car get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return null;
+    }
+
+    @Override
+    public Car update(Car car) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public void addDriverToCar(Driver driver, Car car) {
+
+    }
+
+    @Override
+    public void removeDriverFromCar(Driver driver, Car car) {
+
+    }
+
+    @Override
+    public List<Car> getAllByDriver(Long driverId) {
+        return null;
+    }
+}
