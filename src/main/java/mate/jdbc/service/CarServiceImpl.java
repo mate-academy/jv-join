1f8ca4ef26a5ffabb803bc1980e72c2c14b03820
Olesya Khalak <khalak.olesya.official@gmail.com>
2022-08-29 09:35:16 +0300

    fixed

--- src/main/java/mate/jdbc/service/CarServiceImpl.java
+++ src/main/java/mate/jdbc/service/CarServiceImpl.java
@@ -42,12 +42,16 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public void addDriverToCar(Driver driver, Car car) {
-        carDao.addDriverToCar(driver,car);
+        List<Driver> drivers = car.getDrivers();
+        drivers.add(driver);
+        carDao.update(car);
     }
 
     @Override
     public void removeDriverFromCar(Driver driver, Car car) {
-        carDao.removeDriverFromCar(driver,car);
+        List<Driver> drivers = car.getDrivers();
+        drivers.remove(driver);
+        carDao.update(car);
     }
 
     @Override
