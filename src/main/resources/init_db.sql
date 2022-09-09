a797b86b48dfaa9cbd683973d3d1d750ba7691b3
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-09 14:01:03 +0300

    cosmetic

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -3,18 +3,52 @@ USE `taxi_service`;
 
 DROP TABLE IF EXISTS `manufacturers`;
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+                                `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                `name` VARCHAR(225) NOT NULL,
+                                `country` VARCHAR(225) NOT NULL,
+                                `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                PRIMARY KEY (`id`));
+
+INSERT INTO `manufacturers` (`id`, `name`, `country`) VALUES
+(1,'Acura','Japan'),
+(2,'Alfa Romeo','Italy'),
+(3,'Audi','Germany'),
+(4,'BMW','Germany'),
+(5,'Ferrari','Italy'),
+(6,'SEAT','Spain'),
+(7,'Renault','France'),
+(8,'Peugeot','France'),
+(9,'Fiat','Italy'),
+(10,'Honda','Japan'),
+(11,'Lexus','Japan');
 
 DROP TABLE IF EXISTS `drivers`;
 CREATE TABLE `drivers` (
-                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                  `name` VARCHAR(225) NOT NULL,
-                                  `license_number` VARCHAR(225) NOT NULL,
-                                  `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                  PRIMARY KEY (`id`),
-                                  UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
-                                  UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+                          `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                          `name` VARCHAR(225) NOT NULL,
+                          `license_number` VARCHAR(225) NOT NULL,
+                          `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                          PRIMARY KEY (`id`),
+                          UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
+                          UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+INSERT INTO `drivers` (`id`, `name`, `license_number`) VALUES
+(1,'Ihor','123456789'),
+(2,'Oleg','852741963');
+
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars` (
+                      `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                      `name` VARCHAR(225) NOT NULL,
+                      `license_number` VARCHAR(225) NOT NULL,
+                      `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                      PRIMARY KEY (`id`),
+                      UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
+                      UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+INSERT INTO `drivers` (`id`, `name`, `license_number`) VALUES
+(1,'Ihor','123456789'),
+(2,'Oleg','852741963');
+
+SELECT * FROM taxi_service.manufacturers;
+SELECT * FROM taxi_service.drivers;
