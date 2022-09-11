e696bdb80e0164611d8a922c4c1e5cd440e28efd
Ilja Nekrituk <Nekrit99@gmail.com>
2022-09-11 21:49:26 +0300

    CarService pushed

---
+++ src/main/java/mate/jdbc/service/CarSerice.java
@@ -0,0 +1,24 @@
+package mate.jdbc.service;
+
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+
+public interface CarSerice {
+    Car create(Car car);
+
+    Car get(Long id);
+
+    List<Car> getAll();
+
+    Car update(Car car);
+
+    boolean delete(Long id);
+
+    void addDriverToCar(Driver driver, Car car);
+
+    void removeDriverFromCar(Driver driver, Car car);
+
+    List<Car> getAllByDriver(Long driverId);
+}
