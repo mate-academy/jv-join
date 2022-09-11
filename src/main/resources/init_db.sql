65252a690fb7c49b276631b047d7d4a525f2b551
Alexhqolma <66127994+Alexhqolma@users.noreply.github.com>
2022-09-11 19:27:26 +0300

    Corrected database file

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,18 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars` (
+  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `manufacturer_id` int(11) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `is_deleted` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `cars_drivers`;
+CREATE TABLE `cars_drivers` (
+  `driver_id` BIGINT(11) DEFAULT NULL,
+  `car_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
