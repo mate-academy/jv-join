20b344bf410bf2b09b5d095dce09c069836ee6bb
Ilja Nekrituk <Nekrit99@gmail.com>
2022-09-11 21:48:45 +0300

    All packages and it's files added, create ConnectionUtil

---
+++ src/main/java/mate/jdbc/model/Car.java
@@ -0,0 +1,13 @@
+package mate.jdbc.model;
+
+import java.util.List;
+
+public class Car {
+    private Long id;
+    private String model;
+    private Manufacturer manufacturer;
+    private List<Driver> drivers;
+
+
+
+}
