23f993b3c90b53638ebc2dc921404e2fd3118e8a
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-09-11 16:45:36 +0300

    init_db.sql was changed

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,15 +1,11 @@
 CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
 USE `taxi_service`;
 
-DROP TABLE IF EXISTS `manufacturers`;
-CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
-
+DROP TABLE IF EXISTS `cars_drivers`;
 DROP TABLE IF EXISTS `drivers`;
+DROP TABLE IF EXISTS `cars`;
+DROP TABLE IF EXISTS `manufacturers`;
+
 CREATE TABLE `drivers` (
                                   `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
                                   `name` VARCHAR(225) NOT NULL,
@@ -19,7 +15,13 @@ CREATE TABLE `drivers` (
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
 
-DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `manufacturers` (
+                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                  `name` VARCHAR(225) NOT NULL,
+                                  `country` VARCHAR(225) NOT NULL,
+                                  `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                   PRIMARY KEY (`id`));
+
 CREATE TABLE `taxi_service`.`cars` (
                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
                                  `manufacturer_id` BIGINT(11) NULL,
@@ -32,7 +34,6 @@ CREATE TABLE `taxi_service`.`cars` (
                                                  ON DELETE NO ACTION
                                                  ON UPDATE NO ACTION);
 
-DROP TABLE IF EXISTS `cars_drivers`;
 CREATE TABLE `taxi_service`.`cars_drivers`(
                                   `driver_id`    BIGINT(11) NULL,
                                   `car_id`       BIGINT(11) NULL,
