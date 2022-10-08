49b572db005fa2d816af4a3b030d13d212736acf
DooMLeoN <levberezov01@gmail.com>
2022-10-08 13:27:04 +0300

    added fixes

--- src/main/java/mate/jdbc/dao/CarDaoImpl.java
+++ src/main/java/mate/jdbc/dao/CarDaoImpl.java
@@ -82,6 +82,7 @@ public class CarDaoImpl implements CarDao {
     public Car update(Car car) {
         String query = "UPDATE cars SET manufacturer_id = ?, model = ? "
                 + "WHERE id = ? AND is_deleted = FALSE";
+        deleteAllRelations(car.getId());
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setLong(1, car.getManufacturer().getId());
@@ -93,7 +94,6 @@ public class CarDaoImpl implements CarDao {
             throw new DataProcessingException("Couldn't update "
                     + car + " in DB.", e);
         }
-        deleteAllRelations(car.getId());
         return car;
     }
 
