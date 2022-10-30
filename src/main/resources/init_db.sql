37f54fd5319bed6510f67893c2cf92261fb5291f
Oleksandr <dovy_al@ukr.net>
2022-10-30 17:46:03 +0200

    replace init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,13 +1,13 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
+CREATE SCHEMA IF NOT EXISTS `taxi_db` DEFAULT CHARACTER SET utf8;
 USE `taxi_service`;
 
 DROP TABLE IF EXISTS `manufacturers`;
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                  `name` VARCHAR(225) NOT NULL,
+                                  `country` VARCHAR(225) NOT NULL,
+                                  `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                  PRIMARY KEY (`id`));
 
 DROP TABLE IF EXISTS `drivers`;
 CREATE TABLE `drivers` (
@@ -18,3 +18,18 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+CREATE TABLE `cars` (
+                                  `id` bigint NOT NULL AUTO_INCREMENT,
+                                  `model` varchar(255) DEFAULT NULL,
+                                  `is_deleted` tinyint NOT NULL DEFAULT '0',
+                                  `manufacturer_id` bigint DEFAULT NULL,
+                                  PRIMARY KEY (`id`),
+                                  KEY `cars_manufacturers_fk` (`manufacturer_id`),
+                                  CONSTRAINT `cars_manufacturers_fk` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `cars_drivers` (
+                                  `car_id` bigint NOT NULL,
+                                  `driver_id` bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
