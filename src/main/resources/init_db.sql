86cb384d6282e9b7a7a403fdc5ea7c9ab3b8f95e
shmax <mshveda28@gmail.com>
2022-11-04 14:02:01 +0200

    Fix indentation in init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -3,21 +3,21 @@ USE `taxi_service`;
 
 DROP TABLE IF EXISTS `manufacturer`;
 CREATE TABLE `manufacturer` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+                                `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                `name` VARCHAR(225) NOT NULL,
+                                `country` VARCHAR(225) NOT NULL,
+                                `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                PRIMARY KEY (`id`));
 
 DROP TABLE IF EXISTS `driver`;
 CREATE TABLE `driver` (
-                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                  `name` VARCHAR(225) NOT NULL,
-                                  `license_number` VARCHAR(225) NOT NULL,
-                                  `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                  PRIMARY KEY (`id`),
-                                  UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
-                                  UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+                          `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                          `name` VARCHAR(225) NOT NULL,
+                          `license_number` VARCHAR(225) NOT NULL,
+                          `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                          PRIMARY KEY (`id`),
+                          UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
+                          UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
 
 DROP TABLE IF EXISTS `car`;
 CREATE TABLE `car` (
