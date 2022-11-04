8245b2f5d4185b13d83f1c752e2012a38d2409f8
shmax <mshveda28@gmail.com>
2022-11-04 11:19:04 +0200

    Replace boilerplate model code with lombok annotations

--- src/main/java/mate/jdbc/model/Car.java
+++ src/main/java/mate/jdbc/model/Car.java
@@ -1,96 +1,14 @@
 package mate.jdbc.model;
 
 import java.util.List;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Car {
     private Long id;
     private String model;
     private Manufacturer manufacturer;
     private List<Driver> drivers;
-
-    public Car(Long id, String model, Manufacturer manufacturer, List<Driver> drivers) {
-        this.id = id;
-        this.model = model;
-        this.manufacturer = manufacturer;
-        this.drivers = drivers;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Car setId(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public Car setModel(String model) {
-        this.model = model;
-        return this;
-    }
-
-    public Manufacturer getManufacturer() {
-        return manufacturer;
-    }
-
-    public Car setManufacturer(Manufacturer manufacturer) {
-        this.manufacturer = manufacturer;
-        return this;
-    }
-
-    public List<Driver> getDrivers() {
-        return drivers;
-    }
-
-    public Car setDrivers(List<Driver> drivers) {
-        this.drivers = drivers;
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Car car = (Car) o;
-
-        if (!Objects.equals(id, car.id)) {
-            return false;
-        }
-        if (!Objects.equals(model, car.model)) {
-            return false;
-        }
-        if (!Objects.equals(manufacturer, car.manufacturer)) {
-            return false;
-        }
-        return Objects.equals(drivers, car.drivers);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (model != null ? model.hashCode() : 0);
-        result = 31 * result + (manufacturer != null ? manufacturer.hashCode() : 0);
-        result = 31 * result + (drivers != null ? drivers.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Car{" +
-                "id=" + id +
-                ", model='" + model + '\'' +
-                ", manufacturer=" + manufacturer +
-                ", drivers=" + drivers +
-                '}';
-    }
 }
--- src/main/java/mate/jdbc/model/Driver.java
+++ src/main/java/mate/jdbc/model/Driver.java
@@ -1,67 +1,12 @@
 package mate.jdbc.model;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Driver {
     private Long id;
     private String name;
     private String licenseNumber;
-
-    public Driver(Long id, String name, String licenseNumber) {
-        this.id = id;
-        this.name = name;
-        this.licenseNumber = licenseNumber;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLicenseNumber() {
-        return licenseNumber;
-    }
-
-    public void setLicenseNumber(String licenseNumber) {
-        this.licenseNumber = licenseNumber;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Driver driver = (Driver) o;
-        return Objects.equals(id, driver.id)
-                && Objects.equals(name, driver.name)
-                && Objects.equals(licenseNumber, driver.licenseNumber);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, licenseNumber);
-    }
-
-    @Override
-    public String toString() {
-        return "Driver{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", licenseNumber='" + licenseNumber + '\''
-                + '}';
-    }
 }
