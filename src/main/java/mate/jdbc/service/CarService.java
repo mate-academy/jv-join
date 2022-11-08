f423f85f75f07b3713b939514d3ff80bcd205f4c
unknown <streetreadger@gmail.com>
2022-11-08 13:36:44 +0200

    Add CarService and prepare CarDao

---
+++ src/main/java/mate/jdbc/service/CarService.java
@@ -0,0 +1,24 @@
+package mate.jdbc.service;
+
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+
+public interface CarService {
+    Car create(Car car);
+
+    Car get(Long id);
+
+    List<Car> getAll();
+
+    Car update(Car car);
+
+    boolean delete(Long id);
+
+    void addDriverFromCar(Driver driver, Car car);
+
+    void removeDriverFromCar(Driver driver, Car car);
+
+    List<Car> getAllByDriver(Long driverId);
+}
