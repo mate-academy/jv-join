4caee0f0edebb9ad3c9ebd2ffcbea3be6d4b59cc
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-11-09 10:16:40 +0200

    corrected init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,29 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+/* create table cars */
+  CREATE TABLE `taxi_db`.`cars` (
+                                  `id` BIGINT NOT NULL,
+                                  `model` VARCHAR(255) NOT NULL,
+                                  `manufacturer_id` BIGINT NOT NULL,
+                                  `drivers_id` BIGINT NOT NULL,
+                                  `is_deleted` TINYINT NOT NULL,
+                                  PRIMARY KEY (`id`),
+                                  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+
+/* Create relations */
+ ALTER TABLE `taxi_db`.`cars`
+                                  ADD INDEX `drivers_id_idx` (`drivers_id` ASC) VISIBLE,
+                                  ADD INDEX `manufacturer_id_idx` (`manufacturer_id` ASC) VISIBLE;
+ ALTER TABLE `taxi_db`.`cars`
+                                  ADD CONSTRAINT `manufacturer_id`
+                                  FOREIGN KEY (`manufacturer_id`)
+                                  REFERENCES `taxi_db`.`manufacturers` (`id`)
+                                  ON DELETE NO ACTION
+                                  ON UPDATE NO ACTION,
+                                  ADD CONSTRAINT `drivers_id`
+                                  FOREIGN KEY (`drivers_id`)
+                                  REFERENCES `taxi_db`.`drivers` (`id`)
+                                  ON DELETE NO ACTION
+                                  ON UPDATE NO ACTION;
