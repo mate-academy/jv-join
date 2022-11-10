ef8b4d3c90942e36e2bcd312769f60e77e9b18ba
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-11-10 11:36:16 +0200

    created class Car

--- src/main/java/mate/jdbc/model/Car.java
+++ src/main/java/mate/jdbc/model/Car.java
@@ -1,7 +1,56 @@
 package mate.jdbc.model;
 
+import java.util.Objects;
+
 public class Car {
     private Long id;
     private String model;
-    private
+
+    public Car() {
+    }
+    public Car(Long id, String model) {
+        this.id = id;
+        this.model = model;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(id, car.id) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "id=" + id
+                + ", model='" + model + '\''
+                + '}';
+    }
 }
