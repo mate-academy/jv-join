7f926f719e7fb5738a51f74dad84716c3722933e
Volodymyr Oliinyk <volodymyr.oliinyk89@gmail.com>
2022-11-11 07:36:40 +0200

    implemented task

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 public class ConnectionUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/manufacturers_db";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "doom+8461299";
+    private static final String PASSWORD = "123456";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
