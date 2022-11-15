38827dac1058c0ec7773002ca44b6c7cb33c04f7
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-11-15 23:56:33 +0200

    CarDaoImpl get, delete are implemented

--- src/main/java/mate/jdbc/dao/CarDaoImpl.java
+++ src/main/java/mate/jdbc/dao/CarDaoImpl.java
@@ -3,12 +3,14 @@ package mate.jdbc.dao;
 import mate.jdbc.exception.DataProcessingException;
 import mate.jdbc.lib.Dao;
 import mate.jdbc.model.Car;
+import mate.jdbc.model.Manufacturer;
 import mate.jdbc.util.ConnectionUtil;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,9 +24,10 @@ public class CarDaoImpl implements CarDao {
                      query, Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, car.getModel());
             statement.setLong(2, car.getManufacturer().getId());
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                car.setId(resultSet.getObject(1, Long.class));
+            statement.executeUpdate();
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                car.setId(generatedKeys.getObject(1, Long.class));
             }
             return car;
         } catch (SQLException e) {
@@ -34,12 +37,43 @@ public class CarDaoImpl implements CarDao {
 
     @Override
     public Optional<Car> get(Long id) {
-        return Optional.empty();
+        String query = "SELECT c.id, c.model, m.id, m.name, m.country "
+                + "FROM taxi_service.cars c "
+                + "INNER JOIN taxi_service.manufacturers m "
+                + "ON c.manufacturer_id = m.id "
+                + "WHERE c.id = ? AND c.is_deleted = FALSE;";
+        try (Connection connection = ConnectionUtil.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            Car car = null;
+            if (resultSet.next()) {
+                car = getCar(resultSet);
+            }
+            return Optional.ofNullable(car);
+        } catch (SQLException e) {
+            throw new DataProcessingException("Couldn't get a car with id: " + id, e);
+        }
     }
 
     @Override
     public List<Car> getAll() {
-        return null;
+        String query = "SELECT c.id, c.model, m.id, m.name, m.country "
+                + "FROM taxi_service.cars c "
+                + "INNER JOIN taxi_service.manufacturers m "
+                + "ON c.manufacturer_id = m.id "
+                + "WHERE c.is_deleted = FALSE;";
+        try (Connection connection = ConnectionUtil.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query)) {
+            ResultSet resultSet = statement.executeQuery();
+            List<Car> carList = new ArrayList<>();
+            while (resultSet.next()) {
+                carList.add(getCar(resultSet));
+            }
+            return carList;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Couldn't get a list of cars", e);
+        }
     }
 
     @Override
@@ -49,6 +83,24 @@ public class CarDaoImpl implements CarDao {
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        String query = "UPDATE taxi_service.cars SET taxi_service.cars.is_deleted = TRUE "
+                + "WHERE taxi_service.cars.id = ?;";
+        try (Connection connection = ConnectionUtil.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setLong(1, id);
+            return statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Couldn't delete a car with id: " + id, e);
+        }
+    }
+
+    private Car getCar(ResultSet resultSet) throws SQLException {
+        Long id = resultSet.getObject(1, Long.class);
+        String model = resultSet.getString(2);
+        Long modelId = resultSet.getObject(3, Long.class);
+        String name = resultSet.getString(4);
+        String country = resultSet.getString(5);
+        Manufacturer manufacturer = new Manufacturer(modelId, name, country);
+        return new Car(id, model, manufacturer, null);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,13 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+CREATE TABLE `cars_drivers` (
+                                `driver_id` bigint NOT NULL,
+                                `car_id` bigint NOT NULL,
+                                KEY `driver_id_idx` (`driver_id`),
+                                KEY `cars_id_idx` (`car_id`),
+                                CONSTRAINT `cars_id` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`),
+                                CONSTRAINT `driver_id` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
