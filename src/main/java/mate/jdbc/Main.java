78a3abb25ab5d11f5c12a98f76bafcff7afc7a8f
Oleksandr <oleksandrkorniychuk98@gmail.com>
2022-11-15 17:13:20 +0200

    realized joining tables...

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,45 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService =
+                (ManufacturerService) INJECTOR.getInstance(ManufacturerService.class);
+        Manufacturer fordManufacturer = new Manufacturer("Ford Motor", "USA");
+        Manufacturer porscheManufacturer = new Manufacturer("Porsche", "Germany");
+        manufacturerService.create(fordManufacturer);
+        manufacturerService.create(porscheManufacturer);
+
+        DriverService driverService = (DriverService) INJECTOR.getInstance(DriverService.class);
+        Driver johnDriver = new Driver("John", "ued1234567");
+        Driver nickDriver = new Driver("Nick", "prh6543210");
+        Driver alexDriver = new Driver("Alex", "emq9632580");
+        Driver denisDriver = new Driver("Denis", "yye32191530");
+        driverService.create(johnDriver);
+        driverService.create(nickDriver);
+        driverService.create(alexDriver);
+        driverService.create(denisDriver);
+
+        CarService carService = (CarService) INJECTOR.getInstance(CarService.class);
+        Car fordCar = new Car("Capri", fordManufacturer,
+                List.of(johnDriver, alexDriver));
+        Car porscheCar = new Car("GT4 RS", porscheManufacturer,
+                List.of(nickDriver, alexDriver));
+        carService.create(fordCar);
+        carService.create(porscheCar);
+        carService.removeDriverFromCar(nickDriver, fordCar);
+        carService.addDriverToCar(denisDriver, fordCar);
+        carService.get(fordCar.getId());
+        carService.getAllByDriver(alexDriver.getId()).forEach(System.out::println);
     }
 }
---
+++ src/main/java/mate/jdbc/model/Car.java
@@ -0,0 +1,91 @@
+package mate.jdbc.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Car {
+    private Long id;
+    private String model;
+    private Manufacturer manufacturer;
+    private List<Driver> drivers;
+
+    public Car() {
+    }
+
+    public Car(String name, Manufacturer manufacturer, List<Driver> drivers) {
+        this.model = name;
+        this.manufacturer = manufacturer;
+        this.drivers = drivers;
+    }
+
+    public Car(Long id, String name, Manufacturer manufacturer, List<Driver> drivers) {
+        this.id = id;
+        this.model = name;
+        this.manufacturer = manufacturer;
+        this.drivers = drivers;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public List<Driver> getDrivers() {
+        return drivers;
+    }
+
+    public void setDrivers(List<Driver> drivers) {
+        this.drivers = drivers;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Car car = (Car) obj;
+        return Objects.equals(id, car.id)
+                && Objects.equals(model, car.model)
+                && Objects.equals(manufacturer, car.manufacturer)
+                && Objects.equals(drivers, car.drivers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model, manufacturer, drivers);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb;
+        sb = new StringBuilder("Car{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(model).append('\'');
+        sb.append(", manufacturer=").append(manufacturer);
+        sb.append(", drivers=").append(drivers);
+        sb.append('}');
+        return sb.toString();
+    }
+}
