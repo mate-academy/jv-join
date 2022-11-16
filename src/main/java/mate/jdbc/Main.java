9d4229a9f73be64396a124865c38bac0c6ce9ad4
Luka Lozovyk <lukalozovyk@gmail.com>
2022-11-17 00:54:25 +0200

    Car dao and service implemented.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,64 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+        Manufacturer manufacturerVW = new Manufacturer("Volkswagen", "Germany");
+        Manufacturer manufacturerPorsche = new Manufacturer("Porsche", "Germany");
+        manufacturerService.create(manufacturerVW);
+        manufacturerService.create(manufacturerPorsche);
+
+        DriverService driverService = (DriverService)
+                injector.getInstance(DriverService.class);
+        Driver driver1 = new Driver("Bob", "11111");
+        Driver driver2 = new Driver("Alice", "22222");
+        Driver driver3 = new Driver("Barry", "33333");
+        Driver driver4 = new Driver("Elton", "44444");
+        Driver driver5 = new Driver("Ringo", "55555");
+        driverService.create(driver1);
+        driverService.create(driver2);
+        driverService.create(driver3);
+        driverService.create(driver4);
+        driverService.create(driver5);
+        List<Driver> drivingVwList = List.of(driver1, driver2, driver3);
+        List<Driver> drivingPorscheList = List.of(driver4, driver5);
+
+        CarService carService = (CarService)
+                injector.getInstance(CarService.class);
+        Car porsche911 = new Car("911", manufacturerPorsche, drivingPorscheList);
+        Car vwJetta = new Car("Jetta", manufacturerVW, drivingVwList);
+        carService.create(porsche911);
+        carService.create(vwJetta);
+
+        System.out.println("Car dao and service testing:");
+        System.out.println("Currently in db");
+        System.out.println(carService.getAll());
+
+        vwJetta.setModel("Passat");
+        carService.update(vwJetta);
+        Car updated = carService.get(2L);
+        System.out.println("Updated car with Passat model " + updated);
+
+        carService.addDriverToCar(driver1, porsche911);
+        System.out.println("Car with added driver " + carService.get(1L));
+
+        carService.removeDriverFromCar(driver2, vwJetta);
+        System.out.println("Car without driver2 " + carService.get(2L));
+
+        carService.delete(2L);
+        System.out.println("DB after all changes");
+        System.out.println(carService.getAll());
     }
 }
