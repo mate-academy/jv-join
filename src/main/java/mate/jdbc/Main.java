437b0db5e2867cb853c4135e6fb226b0c3bce8d2
Yulia Tsariv <purplesquirrel68@gmail.com>
2022-11-16 20:32:53 +0200

    Create JV Join

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,39 @@
 package mate.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        Manufacturer manufacturer = new Manufacturer(3L,"Audi","German");
+        List<Driver> drivers = new ArrayList<>();
+        Car car = new Car("audi",manufacturer,drivers);
+
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        carService.create(car);
+
+        Car car1 = carService.get(3L);
+        System.out.println(car1);
+
+        car1.setModel(" Volkswagen");
+        carService.update(car1);
+        System.out.println(carService.get(car1.getId()));
+
+        System.out.println(carService.delete(2L));
+
+        Driver driver = new Driver();
+        driver.setName("Yulia");
+        carService.addDriverToCar(driver, car);
+        List<Car> allByDriver = carService.getAllByDriver(2L);
+        System.out.println(allByDriver);
+        carService.getAll().forEach(System.out::println);
     }
 }
+
