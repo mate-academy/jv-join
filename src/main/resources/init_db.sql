f00b412f21cfcbd430ae90236279cd8df7634164
yehbreus <yeh.breus@gmail.com>
2022-11-16 12:25:12 +0200

    task complete

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,29 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars` (
+  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(225) NOT NULL,
+  `manufacturer_id` BIGINT(11) NULL,
+  `is_deleted` TINYINT NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `cars_manufacturers_fk`
+	FOREIGN KEY (`manufacturer_id`)
+	REFERENCES `manufacturers` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+DROP TABLE IF EXISTS `cars_drivers`;
+CREATE TABLE `cars_drivers` (
+  `car_id` BIGINT(11) NOT NULL,
+  `driver_id` BIGINT(11) NOT NULL,
+  CONSTRAINT `cars_drivers_cars_fk`
+	FOREIGN KEY (`car_id`)
+    REFERENCES `cars` (`id`),
+  CONSTRAINT `cars_drivers_drivers_fk`
+	FOREIGN KEY (`driver_id`)
+    REFERENCES `drivers` (`id`)
+);
