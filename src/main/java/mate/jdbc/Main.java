70d1d765e94c1f5919702e2fef582b7684a54528
MuzykaVP <muzykavp.work@gmail.com>
2022-11-16 18:16:25 +0200

    suggested fixes

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -14,27 +14,32 @@ public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
-        Long testValue = 6L;
         ManufacturerService manufacturerService =
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
-        Manufacturer manufacturer = manufacturerService.get(testValue);
+        Manufacturer manufacturer = manufacturerService
+                .create(new Manufacturer(null, "Ford", "USA"));
         DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
-        Driver driver = driverService.get(testValue);
-        Driver driver1 = driverService.get(testValue + 3);
-        Driver driver3 = driverService.get(testValue + 2);
+        Driver driver = driverService
+                .create(new Driver(null, "VLad", "3343JJI"));
+        Driver driver1 = driverService
+                .create(new Driver(null, "Bohdan", "111111Q"));
+        Driver driver2 = driverService
+                .create(new Driver(null, "Katerina", "4344F3F"));
         List<Driver> drivers = new ArrayList<>();
         drivers.add(driver);
         drivers.add(driver1);
-        Car car = new Car(null, "model" + testValue, manufacturer,
-                drivers);
+        Car car = new Car(null, "model0", manufacturer,drivers);
+        drivers.add(driver2);
+        Car car1 = new Car(null, "model1", manufacturer,drivers);
         CarService carService = (CarService) injector.getInstance(CarService.class);
         Car car2 = carService.create(car);
-        Car car1 = carService.get(3L);
-        carService.addDriverToCar(driver3, car1);
-        Car update = carService.update(car1);
+        Car car3 = carService.create(car1);
+        Car car4 = carService.get(1L);
+        carService.addDriverToCar(driver2, car2);
+        Car update = carService.update(car2);
         boolean delete = carService.delete(1L);
         List<Car> all = carService.getAll();
-        List<Car> allByDriver = carService.getAllByDriver(3L);
-        carService.removeDriverFromCar(driver, car1);
+        List<Car> allByDriver = carService.getAllByDriver(1L);
+        carService.removeDriverFromCar(driver2, car3);
     }
 }
