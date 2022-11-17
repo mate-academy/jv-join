e67ef101cf8a17fc2b9d4ae3121212144ecf9f62
Sidir0m <romangolovatyi@yahoo.com>
2022-11-17 12:02:54 +0200

    Added Car logic

--- src/main/java/mate/jdbc/service/impl/CarServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/CarServiceImpl.java
@@ -1,2 +1,66 @@
-package mate.jdbc.service.impl;public class CarServiceImpl {
+package mate.jdbc.service.impl;
+
+import mate.jdbc.dao.CarDao;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.lib.Service;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.CarService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    @Inject
+    CarDao carDao;
+
+    @Override
+    public Car create(Car car) {
+        return carDao.create(car);
+    }
+
+    @Override
+    public Car get(Long id) {
+        return carDao.get(id).orElseThrow(
+                () -> new NoSuchElementException("Couldn't find a car with id " + id));
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return carDao.getAll();
+    }
+
+    @Override
+    public Car update(Car car) {
+        return carDao.update(car);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return carDao.delete(id);
+    }
+
+    @Override
+    public void addDriverToCar(Driver driver, Car car) {
+        List<Driver> drivers = new ArrayList<>(car.getDrivers());
+        drivers.add(driver);
+        car.setDrivers(drivers);
+        update(car);
+    }
+
+    @Override
+    public void removeDriverFromCar(Driver driver, Car car) {
+        List<Driver> drivers = new ArrayList<>(car.getDrivers());
+        drivers.remove(driver);
+        car.setDrivers(drivers);
+        update(car);
+    }
+
+    @Override
+    public List<Car> getAllByDriver(Long driverId) {
+        return carDao.getAllByDriver(driverId);
+    }
 }
