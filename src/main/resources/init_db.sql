abe7ce9e6d968f6106ed0ca3d63e9dcea2cd6c72
Pavlo Hryhorash <pavlo.hryhorash@gmail.com>
2022-11-17 03:39:50 +0200

    solved jv join task

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,20 +1,53 @@
 CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
+USE
+`taxi_service`;
 
 DROP TABLE IF EXISTS `manufacturers`;
-CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+CREATE TABLE `manufacturers`
+(
+    `id`         BIGINT(11) NOT NULL AUTO_INCREMENT,
+    `name`       VARCHAR(225) NOT NULL,
+    `country`    VARCHAR(225) NOT NULL,
+    `is_deleted` TINYINT      NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
+);
 
 DROP TABLE IF EXISTS `drivers`;
-CREATE TABLE `drivers` (
-                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                  `name` VARCHAR(225) NOT NULL,
-                                  `license_number` VARCHAR(225) NOT NULL,
-                                  `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                  PRIMARY KEY (`id`),
-                                  UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
-                                  UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+CREATE TABLE `drivers`
+(
+    `id`             BIGINT(11) NOT NULL AUTO_INCREMENT,
+    `name`           VARCHAR(225) NOT NULL,
+    `license_number` VARCHAR(225) NOT NULL,
+    `is_deleted`     TINYINT      NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
+    UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE
+);
+
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars`
+(
+    `id`              BIGINT(11) NOT NULL AUTO_INCREMENT,
+    `manufacturer_id` BIGINT(11) DEFAULT NULL,
+    `model`           VARCHAR(255)     DEFAULT NULL,
+    `is_deleted`      TINYINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `cars_manufacturers_fk`
+        FOREIGN KEY (`manufacturer_id`)
+            REFERENCES `manufacturers` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+);
+
+DROP TABLE IF EXISTS `cars_drivers`;
+CREATE TABLE `cars_drivers`
+(
+    `driver_id` BIGINT(11) DEFAULT NULL,
+    `car_id`    BIGINT(11) DEFAULT NULL,
+    CONSTRAINT `cars_drivers_drivers_fk`
+        FOREIGN KEY (`driver_id`)
+            REFERENCES `drivers` (`id`),
+    CONSTRAINT `cars_drivers_cars_fk`
+        FOREIGN KEY (`car_id`)
+            REFERENCES `cars` (`id`)
+);
