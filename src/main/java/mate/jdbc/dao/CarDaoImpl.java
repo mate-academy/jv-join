d8d7fe0b98bba28349695579d36695bac48770ee
Serhii-Prykhodko <114071354+Serhii-Prykhodko@users.noreply.github.com>
2022-11-17 18:12:44 +0200

    Update src/main/java/mate/jdbc/dao/CarDaoImpl.java
    Co-authored-by: ArchKonstantin <www.kosta.kon@gmail.com>

--- src/main/java/mate/jdbc/dao/CarDaoImpl.java
+++ src/main/java/mate/jdbc/dao/CarDaoImpl.java
@@ -74,7 +74,7 @@ public class CarDaoImpl implements CarDao {
         } catch (SQLException e) {
             throw new DataProcessingException("Couldn't get cars", e);
         }
-        cars.forEach(c -> c.setDrivers(getDriversByCarId(c.getId())));
+        cars.forEach(car -> car.setDrivers(getDriversByCarId(c.getId())));
         return cars;
     }
 
