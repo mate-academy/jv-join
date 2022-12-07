1240d45f0fd146a9f2cb7b3320bec1e6f079ffa8
AndriySichniy <sichniy.andriy@gmail.com>
2022-12-07 12:07:51 +0200

    added car carservice and carservise

---
+++ src/main/java/mate/jdbc/model/Car.java
@@ -0,0 +1,10 @@
+package mate.jdbc.model;
+
+import java.util.List;
+
+public class Car {
+    private long id;
+    private String model;
+    private Manufacturer manufacturer;
+    private List<Driver> drivers;
+}
