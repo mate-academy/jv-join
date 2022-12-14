99d29d6b2d9dd0f0a8b4d7d4fe273dc831b5fc47
Maks Hrytsyna <hrytsynamax@gmail.com>
2022-12-15 00:48:34 +0200

    car service is implemented

---
+++ src/main/java/mate/jdbc/service/CarService.java
@@ -0,0 +1,24 @@
+package mate.jdbc.service;
+
+import java.util.List;
+import java.util.Optional;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+
+public interface CarService {
+    Car create(Car car);
+
+    Car get(Long id);
+
+    List<Car> getAll();
+
+    Car update(Car car);
+
+    boolean delete(Long id);
+
+    void addDriverToCar(Driver driver, Car car);
+
+    void removeDriverFromCar(Driver driver, Car car);
+
+    List<Optional<Car>> getAllByDriver(Long driverId);
+}
