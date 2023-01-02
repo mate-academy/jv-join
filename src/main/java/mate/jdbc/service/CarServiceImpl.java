492bdac787f35d779e7765c488d0d5c43072ded3
Danylo Borysenko <borysenkodanyl@gmail.com>
2023-01-02 20:07:17 +0200

    Car model, CarService and CarServiceImpl were added

---
+++ src/main/java/mate/jdbc/service/CarServiceImpl.java
@@ -0,0 +1,47 @@
+package mate.jdbc.service;
+
+import java.util.List;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+
+public class CarServiceImpl implements CarService {
+    @Override
+    public Car create(Car car) {
+        return null;
+    }
+
+    @Override
+    public Car get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return null;
+    }
+
+    @Override
+    public Car update(Car car) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public void addDriverToCar(Driver driver, Car car) {
+
+    }
+
+    @Override
+    public void removeDriverFromCar(Driver driver, Car car) {
+
+    }
+
+    @Override
+    public List<Car> getAllByDriver(Long driverId) {
+        return null;
+    }
+}
