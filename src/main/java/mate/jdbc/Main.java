b22e6ca21723ef105ce7ab8c93473086c795e5c1
Anton Kalenskyi <anton_kalenskiy@ukr.net>
2023-01-03 12:19:23 +0200

    Added CarDao interface with implementation. Also created model Car. In Service package created CarService interface with implementation.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,15 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.service.CarService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        CarService carService =
+                (CarService) injector.getInstance(CarService.class);
+        carService.getAll().forEach(System.out::println);
+        System.out.println(carService.get(1L));
     }
 }
