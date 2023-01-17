ddf41852504871ec33a997490fd114514d15c911
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-01-17 12:50:50 +0000

    Add corrections to task 3.0

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -10,7 +10,6 @@ import mate.jdbc.service.DriverService;
 import mate.jdbc.service.ManufacturerService;
 
 public class Main {
-
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
