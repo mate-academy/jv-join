2ba15bc4ef5e2ae452322f9eea43f59535b922ae
Your NameMykola <mykola.osolinskyi@gmail.com>
2023-01-28 16:32:07 +0100

    init_db done

---
+++ src/main/java/mate/jdbc/dao/CarDao.java
@@ -0,0 +1,19 @@
+package mate.jdbc.dao;
+
+import mate.jdbc.model.Car;
+
+import java.util.List;
+
+public interface CarDao {
+    Car create(Car car);
+
+    Car get(Long id);
+
+    List<Car> getAll();
+
+    Car update(Car car);
+
+    boolean delete(Long id);
+
+    List<Car> getAllByDriver(Long driverId);
+}
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,32 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars` (
+                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                  `model` VARCHAR(225) DEFAULT NULL,
+                                  `manufacturer_id` BIGINT(11) NULL,
+                                  `is_deleted` TINYINT NOT NULL DEFAULT '0',
+                                  PRIMARY KEY (`id`),
+                                  CONSTRAINT 'cars_manufacturers_fk'
+                                                                FOREIGN KEY ('manufacturer_id')
+                                                                REFERENCES 'manufacturers' ('id')
+                                                                ON DELETE NO ACTION
+                                                                ON UPDATE NO ACTION);
+
+DROP TABLE IF EXISTS `cars_drivers`;
+ CREATE TABLE `cars_drivers` (
+                                    `car_id` bigint(11) NOT NULL,
+                                    `driver_id` bigint(11) NOT NULL,
+                                    `is_deleted` tinyint NOT NULL DEFAULT '0',
+                                    CONSTRAINT `cars_drivers_cars_fk`
+                                                                FOREIGN KEY (`car_id`)
+                                                                REFERENCES `cars`(`id`)
+ 						                                        ON DELETE NO ACTION
+                                                                ON UPDATE NO ACTION,
+                                    CONSTRAINT `cars_drivers_drivers_fk`
+                                                                FOREIGN KEY (`driver_id`)
+                                                                REFERENCES `drivers`(`id`)
+   						                                        ON DELETE NO ACTION
+                                                                ON UPDATE NO ACTION);
