ccaa72070ca63e2ac56acb9f074e3d663e5f84f9
Oleksii Zaloznyi <o.zaloznyi@gmail.com>
2023-01-30 00:51:34 +0200

    join task completed

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,45 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver driverOne = new Driver("Giwi", "111");
+        Driver driverTwo = new Driver("Gogi", "222");
+        Driver driverThree = new Driver("Koba", "555");
+        Driver driverFour = new Driver("Ashan", "666");
+        driverService.create(driverOne);
+        driverService.create(driverTwo);
+        driverService.create(driverThree);
+        driverService.create(driverFour);
+        ManufacturerService manufacturerService
+                = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        Manufacturer firstManufacturer = new Manufacturer("Lanos", "Ukraine");
+        Manufacturer secondManufacturer = new Manufacturer("Rolls-Royce", "England");
+        manufacturerService.create(firstManufacturer);
+        manufacturerService.create(secondManufacturer);
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        Car carOne = new Car("lanos", firstManufacturer, List.of(driverFour, driverOne));
+        Car carTwo = new Car("Rolls-Royce", secondManufacturer, List.of(driverThree, driverTwo));
+        carService.create(carOne);
+        carService.create(carTwo);
+        carService.getAll().forEach(System.out::println);
+        carService.removeDriverFromCar(driverFour, carOne);
+        carService.removeDriverFromCar(driverTwo, carTwo);
+        carService.addDriverToCar(driverTwo, carOne);
+        carService.addDriverToCar(driverFour, carTwo);
+        System.out.println(carService.get(carOne.getId()));
+        System.out.println(carService.get(carTwo.getId()));
+        carService.getAllByDriver(driverFour.getId()).forEach(System.out::println);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,24 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+DROP TABLE IF EXISTS cars;
+CREATE TABLE cars (
+	                               id BIGINT NOT NULL AUTO_INCREMENT,
+                                   manufacturer_id BIGINT NOT NULL,
+                                   model VARCHAR(255) NOT NULL,
+                                   is_deleted TINYINT NOT NULL DEFAULT 0,
+                                   PRIMARY KEY (id),
+                                   CONSTRAINT cars_manufacturer_fk
+                                            FOREIGN KEY (manufacturer_id)
+                                            REFERENCES taxi_service.manufacturers (id)
+);
+
+DROP TABLE IF EXISTS cars_drivers;
+CREATE TABLE cars_drivers (
+	                        car_id BIGINT NOT NULL,
+                            driver_id BIGINT,
+
+                            CONSTRAINT cars_drivers_cars_fk FOREIGN KEY (car_id) REFERENCES cars (id),
+                            CONSTRAINT cars_drivers_drivers_fk FOREIGN KEY (driver_id) REFERENCES drivers (id)
+);
