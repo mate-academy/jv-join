5e0b87f951342c8ead489a5218b1b4d326c8b70d
Sergiy Golubchenko <galuba1@gmail.com>
2023-03-13 22:47:42 +0200

    CarDao and CarService was implemented

---
+++ src/main/java/mate/jdbc/dao/CarDao.java
@@ -0,0 +1,20 @@
+package mate.jdbc.dao;
+
+import java.util.List;
+import java.util.Optional;
+import mate.jdbc.model.Car;
+
+public interface CarDao {
+    Car create(Car car);
+
+    Optional<Car> get(Long id);
+
+    List<Car> getAll();
+
+    List<Car> getAllByDriver(Long driverId);
+
+    Car update(Car car);
+
+    boolean delete(Long id);
+
+}
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,23 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+CREATE TABLE `cars` (
+                                  `id` bigint NOT NULL AUTO_INCREMENT,
+                                  `model` varchar(255) DEFAULT NULL,
+                                  `is_deleted` tinyint NOT NULL DEFAULT '0',
+                                  `manufacturer_id` bigint DEFAULT NULL,
+                                  PRIMARY KEY (`id`),
+                                  KEY `cars_manufacturer_fk` (`manufacturer_id`),
+                                  CONSTRAINT `cars_manufacturer_fk` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`)
+                                  ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `cars_drivers` (
+                                  `car_id` bigint NOT NULL,
+                                  `driver_id` bigint NOT NULL,
+                                  KEY `cars_drivers_cars_fr` (`car_id`),
+                                  KEY `cars_drivers_drivers_fr` (`driver_id`),
+                                  CONSTRAINT `cars_drivers_cars_fr` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`),
+                                  CONSTRAINT `cars_drivers_drivers_fr` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`)
+                                  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
