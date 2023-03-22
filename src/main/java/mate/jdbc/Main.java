5e796df4c2bcc8fdefd78ea7fd58f4424a08bc38
Sergiy Golubchenko <galuba1@gmail.com>
2023-03-22 14:53:03 +0200

    fix one field

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -11,8 +11,8 @@ import mate.jdbc.model.Driver;
 import mate.jdbc.service.CarService;
 
 public class Main {
-    private static Injector injector = Injector.getInstance("mate.jdbc");
-    private static CarService carService
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+    private static final CarService carService
             = (CarService) injector.getInstance(CarService.class);
 
     public static void main(String[] args) {
