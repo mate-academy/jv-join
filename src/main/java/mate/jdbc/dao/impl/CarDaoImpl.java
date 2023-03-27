65aaa62ef5c74a862fb9ef6581988348d0213774
Artem Grunin <artem.grunin.2006@gmail.com>
2023-03-27 20:14:02 +0300

    implemented CarService

--- src/main/java/mate/jdbc/dao/impl/CarDaoImpl.java
+++ src/main/java/mate/jdbc/dao/impl/CarDaoImpl.java
@@ -1,12 +1,5 @@
 package mate.jdbc.dao.impl;
 
-import mate.jdbc.dao.CarDao;
-import mate.jdbc.exception.DataProcessingException;
-import mate.jdbc.model.Car;
-import mate.jdbc.model.Driver;
-import mate.jdbc.model.Manufacturer;
-import mate.jdbc.util.ConnectionUtil;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -15,7 +8,15 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import mate.jdbc.dao.CarDao;
+import mate.jdbc.exception.DataProcessingException;
+import mate.jdbc.lib.Dao;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.util.ConnectionUtil;
 
+@Dao
 public class CarDaoImpl implements CarDao {
     @Override
     public Car create(Car car) {
