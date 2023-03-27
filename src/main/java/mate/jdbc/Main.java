8d42d4e64e78648565a00f7e273fbb79605554c3
Mariana Polishchuk <tonksmaryan@gmail.com>
2023-03-27 15:44:01 +0300

    [HW-INNER-JOIN] create class Car and CarService interface with implementation

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,47 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.dao.DriverDao;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.dao.impl.DriverDaoImpl;
+import mate.jdbc.dao.impl.ManufacturerDaoImpl;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.CarService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+    private static final CarService carService
+            = (CarService) injector.getInstance(CarService.class);
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerDao manufacturer = new ManufacturerDaoImpl();
+        Car car = new Car("Model X", manufacturer.get(1L).get(),
+                List.of(new Driver(1L, "Artem", "123456"),
+                        new Driver(2L, "Bob", "654321")));
+        Car carCreate = carService.create(car);
+        System.out.println("Create cars: " + carCreate);
+        Car carGet = carService.get(3L);
+        System.out.println("Get car: " + carGet);
+        System.out.println("Get all cars:");
+        List<Car> allCars = carService.getAll();
+        allCars.stream()
+                .forEach(System.out::println);
+        System.out.println("Get all cars by driver:");
+        List<Car> allCarsByDriver = carService.getAllByDriver(1L);
+        allCarsByDriver.stream()
+                .forEach(System.out::println);
+        Car carUpdate = new Car(7L, "Reno", manufacturer.get(10L).get());
+        carUpdate = carService.update(carUpdate);
+        System.out.println("Car after update " + carUpdate);
+        boolean isDeleteCar = carService.delete(8L);
+        System.out.println(isDeleteCar);
+        DriverDao driverDao = new DriverDaoImpl();
+        Driver driver = driverDao.get(2L).get();
+        carService.addDriverToCar(driver, carUpdate);
+        System.out.println(carService.get(carUpdate.getId()));
+        carService.removeDriverFromCar(driver, carUpdate);
+        System.out.println(carService.get(carUpdate.getId()));
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,23 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+CREATE TABLE `cars` (
+  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `model` VARCHAR(255) DEFAULT NULL,
+  `manufacturer_id` BIGINT(11) NOT NULL,
+  `is_deleted` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `car_manufacturer_idx` (`manufacturer_id`),
+  CONSTRAINT `car_manufacturer_fk` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `cars_drivers` (
+  `driver_id` BIGINT(11) NOT NULL,
+  `car_id` BIGINT(11) NOT NULL,
+  KEY `driver_fk_idx` (`driver_id`),
+  KEY `car_fk_idx` (`car_id`),
+  CONSTRAINT `car_fk` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`),
+  CONSTRAINT `driver_fk` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
