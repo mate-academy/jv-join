e201b9b76b4fa1f5d67b9760300f9e8b41e746bc
Oleksii Stepaniak <alexstepanyak@gmail.com>
2023-03-27 19:48:01 +0300

    first version of hw

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,29 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars` (
+                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                  `manufacturer_id` BIGINT,
+                                  `model` VARCHAR(225) NOT NULL,
+                                  `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                  PRIMARY KEY (`id`),
+
+                                  CONSTRAINT `cars_manufacturers_fk`
+                                  FOREIGN KEY (`manufacturer_id`)
+                                  REFERENCES `taxi_service`.`manufacturers` (`id`));
+
+DROP TABLE IF EXISTS `cars_drivers`;
+CREATE TABLE `cars_drivers` (
+                                  `car_id` BIGINT NOT NULL,
+                                  `driver_id` BIGINT NOT NULL,
+
+                                  CONSTRAINT `cars_drivers_cars_fk`
+                                  FOREIGN KEY (`car_id`)
+                                  REFERENCES `cars` (`id`),
+                                  CONSTRAINT `cars_drivers_driver_fk`
+                                  FOREIGN KEY (`driver_id`)
+                                  REFERENCES `drivers` (`id`)
+);
+
