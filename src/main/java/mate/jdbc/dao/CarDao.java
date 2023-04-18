4c469ceb673da1377b3b59f9a4a256bf6f4cc8d7
Artur Muliukov <arthur.muliukov@gmail.com>
2023-04-18 12:24:31 +0200

    Solved 1.0

---
+++ src/main/java/mate/jdbc/dao/CarDao.java
@@ -0,0 +1,20 @@
+package mate.jdbc.dao;
+
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+
+public interface CarDao {
+    Car create(Car car);
+
+    Car get(Long id);
+
+    List<Car> getAll();
+
+    Car update(Car car);
+
+    boolean delete(Long id);
+
+    List<Car> getAllByDriver(Long driverId);
+}
---
+++ src/main/java/mate/jdbc/service/impl/CarServiceImpl.java
@@ -0,0 +1,60 @@
+package mate.jdbc.service.impl;
+
+import mate.jdbc.dao.CarDao;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.lib.Service;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.CarService;
+
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+    @Inject
+    CarDao carDao;
+
+    @Override
+    public Car create(Car car) {
+        return carDao.create(car);
+    }
+
+    @Override
+    public Car get(Long id) {
+        return carDao.get(id);
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return carDao.getAll();
+    }
+
+    @Override
+    public Car update(Car car) {
+        return carDao.update(car);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return carDao.delete(id);
+    }
+
+    @Override
+    public void addDriverToCar(Driver driver, Car car) {
+        if (!car.getDrivers().contains(driver)) {
+            car.getDrivers().add(driver);
+            carDao.update(car);
+        }
+    }
+
+    @Override
+    public void removeDriverFromCar(Driver driver, Car car) {
+        car.getDrivers().remove(driver);
+        carDao.update(car);
+    }
+
+    @Override
+    public List<Car> getAllByDriver(Long driverId) {
+        return carDao.getAllByDriver(driverId);
+    }
+}
