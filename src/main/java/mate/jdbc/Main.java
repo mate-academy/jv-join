78a6a339ecdcccd24afa1679783b67c2076f6093
Denis Kalyakin <denis.kalyakin999@gmail.com>
2023-05-25 20:01:02 +0300

    create, get, getAll and update methods were implemented

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,10 +1,19 @@
 package mate.jdbc;
 
-import java.sql.Connection;
-import mate.jdbc.util.ConnectionUtil;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.ManufacturerService;
 
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
     public static void main(String[] args) {
-        Connection connection = ConnectionUtil.getConnection();
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        System.out.println(carService.get(2L));
+        Car druchok = carService.get(2L);
+        druchok.setModel("Druchok");
+        carService.update(druchok);
+        carService.getAll().forEach(System.out::println);
     }
 }
--- src/main/java/mate/jdbc/service/impl/CarServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/CarServiceImpl.java
@@ -1,6 +1,9 @@
 package mate.jdbc.service.impl;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import mate.jdbc.dao.CarDao;
+import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
 import mate.jdbc.model.Car;
 import mate.jdbc.model.Driver;
@@ -8,29 +11,32 @@ import mate.jdbc.service.CarService;
 
 @Service
 public class CarServiceImpl implements CarService {
+    @Inject
+    private CarDao carDao;
     @Override
     public Car create(Car car) {
-        return null;
+        return carDao.create(car);
     }
 
     @Override
     public Car get(Long id) {
-        return null;
+        return carDao.get(id)
+                .orElseThrow(() -> new NoSuchElementException("Could not get car by id = " + id));
     }
 
     @Override
     public List<Car> getAll() {
-        return null;
+        return carDao.getAll();
     }
 
     @Override
     public Car update(Car car) {
-        return null;
+        return carDao.update(car);
     }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        return carDao.delete(id);
     }
 
     @Override
