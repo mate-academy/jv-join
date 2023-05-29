d738184bd3413f06af5611cf0a1c8d34c80f97e3
Melnychuk <melnychukmyhailo@gmail.com>
2023-05-29 13:11:29 +0300

    Remove unused constructor.

--- src/main/java/mate/jdbc/model/Car.java
+++ src/main/java/mate/jdbc/model/Car.java
@@ -9,13 +9,6 @@ public class Car {
     private Manufacturer manufacturer;
     private List<Driver> drivers;
 
-    public Car(Long id, String model, Manufacturer manufacturer, List<Driver> drivers) {
-        this.id = id;
-        this.model = model;
-        this.manufacturer = manufacturer;
-        this.drivers = drivers;
-    }
-
     public Car() {
     }
 
