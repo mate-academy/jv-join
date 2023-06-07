848492411085678146f973222e5f3cb10c7179a3
Azzzrael <vasyl.gladky@gmail.com>
2023-06-07 20:41:36 +0300

    little fix

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "YOUR DATABASE URL";
-    private static final String USERNAME = "YOUR USERNAME";
-    private static final String PASSWORD = "YOUR PASSWORD";
-    private static final String JDBC_DRIVER = "YOUR DRIVER";
+    private static final String URL = "jdbc:mysql://localhost:3306/taxi_service_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Panamera100";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
         try {
@@ -19,14 +19,10 @@ public class ConnectionUtil {
         }
     }
 
-    public static Connection getConnection() {
+    public static Connection getConnection() throws SQLException {
         Properties dbProperties = new Properties();
         dbProperties.setProperty("user", USERNAME);
         dbProperties.setProperty("password", PASSWORD);
-        try {
-            return DriverManager.getConnection(URL, dbProperties);
-        } catch (SQLException e) {
-            throw new RuntimeException("Can't create connection to DB ", e);
-        }
+        return DriverManager.getConnection(URL, dbProperties);
     }
 }
