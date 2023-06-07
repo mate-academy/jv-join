0c512c4236d8d8ec5df71c472869bbc7ae4b4da8
Stas Zlishchev <zlishchev@gmail.com>
2023-06-07 13:50:23 +0300

    formatted init file and refactored main class

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -17,31 +17,31 @@ public class Main {
         DriverService driverService = (DriverService) INJECTOR.getInstance(DriverService.class);
 
         Driver petro = new Driver("Petro", "4131");
-        Driver ivan = new Driver("Ivan", "3122");
         Driver createdPetro = driverService.create(petro);
-        Driver createdIvan = driverService.create(ivan);
-        Driver newPetro = driverService.get(createdPetro.getId());
+        Driver createdIvan = driverService.create(new Driver("Ivan", "3122"));
         Driver newIvan = driverService.get(createdIvan.getId());
-        System.out.println("Petro: " + newPetro);
+
+        System.out.println("Petro: " + driverService.get(createdPetro.getId()));
         System.out.println("Ivan: " + newIvan);
         List<Driver> puntoDrivers = new ArrayList<>();
         puntoDrivers.add(createdIvan);
         puntoDrivers.add(createdPetro);
+
         ManufacturerService manufacturerService
                 = (ManufacturerService) INJECTOR.getInstance((ManufacturerService.class));
-        Manufacturer fiat = new Manufacturer("Fiat", "Italy");
-        Manufacturer createdFiat = manufacturerService.create(fiat);
+        Manufacturer createdFiat = manufacturerService.create(new Manufacturer("Fiat", "Italy"));
         Manufacturer newFiat = manufacturerService.get(createdFiat.getId());
         CarService carService = (CarService) INJECTOR.getInstance(CarService.class);
         Car punto = new Car("Punto",newFiat, puntoDrivers);
+
         carService.create(punto);
         System.out.println(carService.get(punto.getId()));
         punto.setModel("Punto Grande");
         carService.update(punto);
         printAllCars(carService);
-        Driver semen = new Driver("Semen", "1324");
-        Driver createdSemen = driverService.create(semen);
+        Driver createdSemen = driverService.create(new Driver("Semen", "1324"));
         carService.addDriverToCar(createdSemen, punto);
+
         printAllCars(carService);
         carService.removeDriverFromCar(petro, punto);
         printAllCars(carService);
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -3,42 +3,42 @@ USE `taxi`;
 
 DROP TABLE IF EXISTS `manufacturers`;
 CREATE TABLE `manufacturers` (
-`id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-`name` VARCHAR(225) NOT NULL,
-`country` VARCHAR(225) NOT NULL,
-`is_deleted` TINYINT NOT NULL DEFAULT 0,
-PRIMARY KEY (`id`));
+    `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(225) NOT NULL,
+    `country` VARCHAR(225) NOT NULL,
+    `is_deleted` TINYINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`));
 
 DROP TABLE IF EXISTS `drivers`;
 CREATE TABLE `drivers` (
-`id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-`name` VARCHAR(225) NOT NULL,
-`license_number` VARCHAR(225) NOT NULL,
-`is_deleted` TINYINT NOT NULL DEFAULT 0,
- PRIMARY KEY (`id`),
-UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
-UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+    `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(225) NOT NULL,
+    `license_number` VARCHAR(225) NOT NULL,
+    `is_deleted` TINYINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
+    UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
 
 DROP TABLE IF EXISTS `cars`;
 CREATE TABLE `cars` (
-`id` BIGINT NOT NULL AUTO_INCREMENT,
-`model` VARCHAR(255) NULL,
-`is_deleted` TINYINT NOT NULL DEFAULT 0,
-`manufacturer_id` BIGINT NULL,
-PRIMARY KEY (`id`),
-CONSTRAINT `cars_manufacturers_fk`
-FOREIGN KEY (`manufacturer_id`)
-REFERENCES `taxi_service`.`manufacturers` (`id`)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION);
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `model` VARCHAR(255) NULL,
+    `is_deleted` TINYINT NOT NULL DEFAULT 0,
+    `manufacturer_id` BIGINT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `cars_manufacturers_fk`
+        FOREIGN KEY (`manufacturer_id`)
+        REFERENCES `manufacturers` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION);
 
 DROP TABLE IF EXISTS `cars_drivers`;
 CREATE TABLE `cars_drivers` (
-`car_id` BIGINT NOT NULL,
-`driver_id` BIGINT NOT NULL,
-CONSTRAINT `cars_drivers_cars_fk`
-FOREIGN KEY (`car_id`)
-REFERENCES `taxi_service`.`cars` (`id`),
-CONSTRAINT `cars_drivers_drivers_fk`
-FOREIGN KEY (`driver_id`)
-REFERENCES `taxi_service`.`drivers` (`id`));
+    `car_id` BIGINT NOT NULL,
+    `driver_id` BIGINT NOT NULL,
+    CONSTRAINT `cars_drivers_cars_fk`
+        FOREIGN KEY (`car_id`)
+        REFERENCES `cars` (`id`),
+        CONSTRAINT `cars_drivers_drivers_fk`
+        FOREIGN KEY (`driver_id`)
+        REFERENCES `drivers` (`id`));
