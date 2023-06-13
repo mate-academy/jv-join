5f811206cc6224587069b250f1691588278bfaed
Pavlo Polezhay <pasha200417@gmail.com>
2023-06-13 11:51:52 +0300

    Solved

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,20 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars` (
+                        `id` BIGINT NOT NULL AUTO_INCREMENT,
+                        `model` VARCHAR(45) NOT NULL,
+                        `manufacturer_id` BIGINT NOT NULL,
+                        `is_deleted` TINYINT NOT NULL DEFAULT '0',
+                        PRIMARY KEY (`id`),
+                        CONSTRAINT cars_manufacturers_fk FOREIGN KEY (manufacturer_id) REFERENCES manufacturers(id));
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `cars_drivers`;
+CREATE TABLE `cars_drivers` (
+                                `car_id` BIGINT NOT NULL,
+                                `driver_id` BIGINT NOT NULL,
+                                CONSTRAINT cars_drivers_cars_fk FOREIGN KEY (car_id) REFERENCES cars(id),
+                                CONSTRAINT cars_drivers_drivers_fk FOREIGN KEY (driver_id) REFERENCES drivers(id));
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
