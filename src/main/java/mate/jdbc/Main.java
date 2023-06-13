02c5149c4fb2e18c91837e13b15cc52998f1102b
DVArt <dvart@hotmail.com>
2023-06-14 01:49:20 +0300

    Unit task

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,65 @@
 package mate.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        Driver arthurDriver = new Driver("Arthur", "666");
+        Driver madhooDriver = new Driver("Madhoo", "1933");
+        {
+            DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+            driverServiceTest(driverService, arthurDriver);
+            driverServiceTest(driverService, madhooDriver);
+            driverService.getAll().forEach(System.out::println);
+        }
+        List<Driver> drivers = new ArrayList<>();
+        {
+            drivers.add(arthurDriver);
+            drivers.add(madhooDriver);
+            System.out.println(drivers);
+        }
+        Manufacturer daewooManufacturer = new Manufacturer("Daewoo", "Mongolia");
+        Manufacturer volvoManufacturer = new Manufacturer("Volvo", "OAO");
+        {
+            ManufacturerService manufacturerService =
+                    (ManufacturerService) injector.getInstance(ManufacturerService.class);
+            manufacturerServiceTest(manufacturerService, daewooManufacturer);
+            manufacturerServiceTest(manufacturerService, volvoManufacturer);
+            manufacturerService.getAll().forEach(System.out::println);
+        }
+        Car daewooMatiz = new Car("Daewoo Matiz", daewooManufacturer, drivers);
+        Car volvoEx90 = new Car("Volvo EX90", volvoManufacturer, drivers);
+        {
+            CarService carService = (CarService) injector.getInstance(CarService.class);
+            carServiceTest(carService, daewooMatiz);
+            carServiceTest(carService, volvoEx90);
+            carService.getAll().forEach(System.out::println);
+        }
+    }
+
+    private static void carServiceTest(CarService carService, Car car) {
+        carService.create(car);
+        carService.update(car);
+    }
+
+    private static void manufacturerServiceTest(
+            ManufacturerService manufacturerService, Manufacturer manufacturer) {
+        manufacturerService.create(manufacturer);
+        manufacturerService.update(manufacturer);
+    }
+
+    private static void driverServiceTest(DriverService driverService, Driver driver) {
+        driverService.create(driver);
+        driverService.update(driver);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,20 +1,36 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
-
-DROP TABLE IF EXISTS `manufacturers`;
+DROP TABLE `library_db`.`cars`, `library_db`.`cars_drivers`, `library_db`.`drivers`, `library_db`.`manufacturers`;
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
-
-DROP TABLE IF EXISTS `drivers`;
+                                 `id` bigint(11) NOT NULL AUTO_INCREMENT,
+                                 `name` varchar(45) DEFAULT NULL,
+                                 `country` varchar(45) DEFAULT NULL,
+                                 `is_deleted` tinyint NOT NULL DEFAULT '0',
+                                 PRIMARY KEY (`id`)
+)ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 CREATE TABLE `drivers` (
-                                  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                  `name` VARCHAR(225) NOT NULL,
-                                  `license_number` VARCHAR(225) NOT NULL,
-                                  `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                  PRIMARY KEY (`id`),
-                                  UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
-                                  UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `name` varchar(45) DEFAULT NULL,
+                           `license_number` varchar(45) DEFAULT NULL,
+                           `is_deleted` tinyint NOT NULL DEFAULT '0',
+                           PRIMARY KEY (`id`),
+                           UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
+                           UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE
+)ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `cars` (
+                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                        `manufacturer_id` BIGINT NOT NULL,
+                        `model` VARCHAR(225) NOT NULL,
+                        `is_deleted` TINYINT NOT NULL DEFAULT '0',
+                        PRIMARY KEY (`id`),
+                        KEY `cars_manufacturers_fk` (`manufacturer_id`),
+                        CONSTRAINT `cars_manufacturers_fk` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`)
+)ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `cars_drivers` (
+                                `driver_id` BIGINT NOT NULL,
+                                `car_id` BIGINT NOT NULL,
+                                KEY `cars_drivers_cars_fk` (`car_id`),
+                                KEY `cars_drivers_drivers_fk` (`driver_id`),
+                                CONSTRAINT `cars_drivers_cars_fk` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`),
+                                CONSTRAINT `cars_drivers_drivers_fk` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`)
+)ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
