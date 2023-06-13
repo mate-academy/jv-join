debe3b7c07b88929980d00f5165cdbdc09413951
Pasha <pavlokrasota@gmail.com>
2023-06-13 19:51:48 +0300

    Removed mistake with constant

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -12,47 +12,49 @@ import mate.jdbc.service.ManufacturerService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
-
     private static final ManufacturerService manufacturerService =
             (ManufacturerService) injector.getInstance(ManufacturerService.class);
     private static final CarService carService =
             (CarService) injector.getInstance(CarService.class);
     private static final DriverService driverService =
             (DriverService) injector.getInstance(DriverService.class);
-    private static final Manufacturer fiat =
-            manufacturerService.create(new Manufacturer("Fiat", "France"));
-    private static final Manufacturer skoda =
-            manufacturerService.create(new Manufacturer("Skoda", "Czech"));
-    private static final Driver driverBob =
-            driverService.create(new Driver("Bob", "124512"));
-    private static final Driver driverJohn =
-            driverService.create(new Driver("John", "925031"));
 
     public static void main(String[] args) {
-        driverService.getAll().forEach(System.out::println);
+        Driver driverBob;
+        Driver driverJohn;
+        driverBob = driverService.create(new Driver("Bob", "124512"));
+        driverJohn = driverService.create(new Driver("John", "925031"));
 
         manufacturerService.getAll().forEach(System.out::println);
+        driverService.getAll().forEach(System.out::println);
+
+        Manufacturer skoda;
+        Manufacturer fiat;
+        fiat = manufacturerService.create(new Manufacturer("Fiat", "France"));
+        skoda = manufacturerService.create(new Manufacturer("Skoda", "Czech"));
         skoda.setName("WAG");
 
         List<Driver> drivers = new ArrayList<>();
         drivers.add(driverBob);
         drivers.add(driverJohn);
+
         Car firstCar = new Car("Ducatto", fiat, drivers);
         Car secondCar = new Car("SuperB", skoda, drivers);
+
         carService.create(firstCar);
         carService.create(secondCar);
         System.out.println(carService.get(secondCar.getId()));
-        carService.removeDriverFromCar(driverBob, firstCar);
 
+        carService.removeDriverFromCar(driverBob, firstCar);
         manufacturerService.update(skoda);
         manufacturerService.getAll().forEach(System.out::println);
 
         List<Car> allByDriver = carService.getAllByDriver(driverBob.getId());
         carService.delete(firstCar.getId());
         Manufacturer manufacturer = manufacturerService.get(fiat.getId());
+
         System.out.println(allByDriver);
         System.out.println(manufacturer);
         driverService.getAll().forEach(System.out::println);
-
     }
 }
