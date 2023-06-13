8f13d432ef4c0c24a5d50194daee136ddd5c70de
Володимир <vova.malyarchuk2@gmail.com>
2023-06-13 20:35:58 +0300

    Implemented task JOIN-FIRST

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,40 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+    private static final ManufacturerService manufacturerService
+            = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+    private static final DriverService driverService
+            = (DriverService) injector.getInstance(DriverService.class);
+    private static final CarService carService
+            = (CarService) injector.getInstance(CarService.class);
+    private static final Manufacturer manufacturer = new Manufacturer("LANOS", "UKRAINE");
+
     public static void main(String[] args) {
-        // test your code here
+        manufacturerService.create(manufacturer);
+        Driver stepan = new Driver("Stepan", "123");
+        Driver petro = new Driver("Petro", "321");
+        driverService.create(stepan);
+        driverService.create(petro);
+        System.out.println(driverService.getAll());
+        Car car = new Car("Mitsubishi", manufacturer, List.of(stepan, petro));
+        car.setModel("BMW");
+        car = carService.create(car);
+        car = carService.get(car.getId());
+        carService.removeDriverFromCar(petro, car);
+        carService.getAll().forEach(System.out::println);
+        carService.getAllByDriver(stepan.getId()).forEach(System.out::println);
+        carService.delete(car.getId());
+        carService.getAll().forEach(System.out::println);
     }
 }
