6249f4b5cd8b2fbcfab6b509befb70d3b1043504
pochtalon <pochtalon1010@gmail.com>
2023-07-07 18:45:02 +0300

    Created class Car; Created CarService and CarDao interfaces; Created CarServiceImpl and CarDaoImpl classes; Created table cars and added changes to init_db.sql file; Created table cars_drivers and added changes to init_db.sql file; Implemented methods in CarServiceImpl class; Implemented methods in CarDaoImpl class;

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,84 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+    private static final ManufacturerService manufacturerService =
+            (ManufacturerService) injector.getInstance(ManufacturerService.class);
+    private static final DriverService driverService =
+            (DriverService) injector.getInstance(DriverService.class);
+
+    private static final CarService carService =
+            (CarService) injector.getInstance(CarService.class);
+
     public static void main(String[] args) {
-        // test your code here
+        Manufacturer manufacturerBmw = new Manufacturer();
+        manufacturerBmw.setName("bmw");
+        manufacturerBmw.setCountry("germany");
+
+        Manufacturer manufacturerLexus = new Manufacturer();
+        manufacturerLexus.setName("lexus");
+        manufacturerLexus.setCountry("japan");
+
+        Manufacturer manufacturerFord = new Manufacturer();
+        manufacturerFord.setName("ford");
+        manufacturerFord.setCountry("usa");
+
+        manufacturerService.create(manufacturerBmw);
+        manufacturerService.create(manufacturerLexus);
+        manufacturerService.create(manufacturerFord);
+
+        Driver driverDamien = new Driver();
+        driverDamien.setName("Damien");
+        driverDamien.setLicenseNumber("666");
+
+        Driver driverJim = new Driver();
+        driverJim.setName("Jim");
+        driverJim.setLicenseNumber("23");
+
+        Driver driverVivec = new Driver();
+        driverVivec.setName("Vivec");
+        driverVivec.setLicenseNumber("36");
+
+        driverService.create(driverDamien);
+        driverService.create(driverJim);
+        driverService.create(driverVivec);
+
+        Car bmw = new Car();
+        bmw.setModel("i5");
+        bmw.setManufacturer(manufacturerBmw);
+        bmw.setDrivers(List.of(driverDamien, driverJim));
+
+        Car lexus = new Car();
+        lexus.setModel("lx 570");
+        lexus.setManufacturer(manufacturerLexus);
+        lexus.setDrivers(List.of(driverDamien, driverVivec));
+
+        Car ford = new Car();
+        ford.setModel("mustang");
+        ford.setManufacturer(manufacturerFord);
+        ford.setDrivers(List.of(driverVivec, driverJim));
+
+        carService.create(bmw);
+        carService.create(lexus);
+        carService.create(ford);
+        carService.getAll().forEach(System.out::println);
+        System.out.println(carService.get(ford.getId()));
+
+        ford.setModel("charger");
+        List<Driver> fordDrivers = List.of(driverDamien, driverVivec, driverJim);
+        ford.setDrivers(fordDrivers);
+        carService.update(ford);
+        carService.delete(bmw.getId());
+        carService.getAll().forEach(System.out::println);
+        carService.getAllByDriver(driverDamien.getId()).forEach(System.out::println);
     }
 }
