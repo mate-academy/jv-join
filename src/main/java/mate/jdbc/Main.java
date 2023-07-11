2032877bf7100d3e9e110993a4af1d3143395d54
VictoriaMrkvch <markevuch.viktorija@gmail.com>
2023-07-11 22:18:41 +0300

    solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,20 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Car car = carService.get(3L);
+        Driver driver = driverService.get(2L);
+        carService.addDriverToCar(driver, car);
+        carService.getAll().forEach(System.out::println);
     }
 }
