c4ea1d82eb8eb4c4c3309ad4020c6345c0c1d79a
Yaroslav Kostenko <xsander3@gmail.com>
2023-07-21 16:38:32 +0300

    Car, СarService and CarDao are implemented

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,55 @@
 package mate.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        CarService carService = (CarService) injector.getInstance(CarService.class);
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Car car = carService.get(2L);
+        Driver driver = driverService.get(3L);
+        System.out.println("Test carService.get: ");
+        System.out.println(car.toString());
+        carService.addDriverToCar(driver, car);
+        System.out.println("Test carService.addDriverToCar: ");
+        System.out.println(car);
+        carService.removeDriverFromCar(driver, car);
+        System.out.println("Test carService.removeDriverFromCar: ");
+        System.out.println(car);
+        Car newCar = new Car();
+        ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+        newCar.setManufacturer(manufacturerService.get(1L));
+        newCar.setModel("X1");
+        List<Driver> drivers = new ArrayList<>();
+        drivers.add(driverService.get(1L));
+        drivers.add(driverService.get(2L));
+        drivers.add(driverService.get(3L));
+        newCar.setDrivers(drivers);
+        System.out.println("Test carService.create: ");
+        System.out.println(carService.create(newCar));
+        newCar.setModel("X3");
+        drivers.remove(2);
+        System.out.println("Test carService.update: ");
+        System.out.println(carService.update(newCar));
+        List<Car> cars = carService.getAllByDriver(2L);
+        System.out.println("Test carService.getAllByDriver: ");
+        cars.forEach(c -> System.out.println(c.toString()));
+        cars = carService.getAll();
+        System.out.println("Test carService.getAll: ");
+        cars.forEach(c -> System.out.println(c.toString()));
+        carService.delete(1L);
+        cars = carService.getAll();
+        System.out.println("Test carService.delete: ");
+        cars.forEach(c -> System.out.println(c.toString()));
     }
 }
---
+++ src/main/java/mate/jdbc/service/impl/CarServiceImpl.java
@@ -0,0 +1,77 @@
+package mate.jdbc.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import mate.jdbc.dao.CarDao;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.lib.Service;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.CarService;
+
+@Service
+public class CarServiceImpl implements CarService {
+    @Inject
+    private CarDao carDao;
+
+    @Override
+    public Car create(Car car) {
+        return carDao.create(car);
+    }
+
+    @Override
+    public Car get(Long id) {
+        return carDao.get(id).orElseThrow(() -> new NoSuchElementException("Could not get car "
+                + "by id = " + id));
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return carDao.getAll();
+    }
+
+    @Override
+    public Car update(Car car) {
+        return carDao.update(car);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return carDao.delete(id);
+    }
+
+    @Override
+    public void addDriverToCar(Driver driver, Car car) {
+        List<Driver> drivers = car.getDrivers();
+        drivers.add(driver);
+        car.setDrivers(drivers);
+        carDao.update(car);
+    }
+
+    @Override
+    public void removeDriverFromCar(Driver driver, Car car) {
+        List<Driver> drivers = car.getDrivers();
+        drivers.remove(driver);
+        car.setDrivers(drivers);
+        carDao.update(car);
+    }
+
+    @Override
+    public List<Car> getAllByDriver(Long driverId) {
+        List<Car> cars = carDao.getAll();
+        List<Car> result = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getDrivers() != null) {
+                List<Driver> drivers = car.getDrivers();
+                for (Driver driver : drivers) {
+                    if (driver.getId().equals(driverId)) {
+                        result.add(car);
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
