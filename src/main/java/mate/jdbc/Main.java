ac402de8be9fc88bd5a7f6f8c562463c76feeef1
Yurii Ivanov <ivanovyura76@gmail.com>
2023-07-27 00:09:44 +0300

    Solution of this task

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,38 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+    private static final ManufacturerService manufacturerService = (ManufacturerService)
+            injector.getInstance(ManufacturerService.class);
+    private static final DriverService driverService = (DriverService)
+            injector.getInstance(DriverService.class);
+    private static final CarService carService = (CarService)
+            injector.getInstance(CarService.class);
+
     public static void main(String[] args) {
-        // test your code here
+        Car car = new Car();
+        car.setModel("Jeep");
+        car.setManufacturer(manufacturerService.get(5L));
+        car.setDrivers(List.of(new Driver[]{driverService.get(5L), driverService.get(4L)}));
+        carService.create(car);
+        car = carService.get(6L);
+        car.setDrivers(List.of(new Driver[]{driverService.get(4L), driverService.get(3L)}));
+        carService.update(car);
+        carService.delete(3L);
+        Driver driver;
+        driver = driverService.get(2L);
+        car = carService.get(1L);
+        carService.getAllByDriver(3L);
+        carService.addDriverToCar(driver, car);
+        carService.removeDriverFromCar(driver, car);
+        carService.getAll();
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -18,3 +18,25 @@ CREATE TABLE `drivers` (
                                   PRIMARY KEY (`id`),
                                   UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE,
                                   UNIQUE INDEX `license_number_UNIQUE` (`license_number` ASC) VISIBLE);
+
+CREATE TABLE `cars` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `model` varchar(225) NULL,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  `manufacturer_id` bigint NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `cars_manufacturers_fk`
+  FOREIGN KEY (`manufacturer_id`)
+  REFERENCES `taxi_service_db`.`manufacturers` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+)
+
+CREATE TABLE `cars_drivers` (
+  `cars_id` bigint NOT NULL,
+  `drivers_id` bigint NOT NULL,
+  KEY `cars_drivers_cars_fk` (`cars_id`),
+  KEY `cars_drivers_drivers_fk` (`drivers_id`),
+  CONSTRAINT `cars_drivers_cars_fk` FOREIGN KEY (`cars_id`) REFERENCES `cars` (`id`),
+  CONSTRAINT `cars_drivers_drivers_fk` FOREIGN KEY (`drivers_id`) REFERENCES `drivers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
