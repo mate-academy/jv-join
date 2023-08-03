6af30a7ba53a692b47137d6d133adca93740b47f
Mykyta Hulianytskyi <yadiver82@gmail.com>
2023-08-03 22:04:05 +0300

    hw_jv-join_v1.0 Car, CarDao, CarService created with all needed methods and injector's annotations.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,92 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.CarService;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        final CarService carService = (CarService) injector.getInstance(CarService.class);
+        final DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        final ManufacturerService manufacturerService
+                = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
+        manufacturerService.create(new Manufacturer("Audi", "Germany"));
+        manufacturerService.create(new Manufacturer("BMW", "Germany"));
+        manufacturerService.create(new Manufacturer("Mercedes", "Germany"));
+        manufacturerService.create(new Manufacturer("Tesla", "USA"));
+        System.out.println("\n manufacturers created: ");
+        manufacturerService.getAll().forEach(System.out::println);
+        driverService.create(new Driver("John", "L12"));
+        driverService.create(new Driver("Mike", "L13"));
+        driverService.create(new Driver("Bob", "L14"));
+        driverService.create(new Driver("Tom", "L15"));
+        driverService.create(new Driver("Jack", "L16"));
+        driverService.create(new Driver("Nik", "L17"));
+        System.out.println("\n drivers created: ");
+        driverService.getAll().forEach(System.out::println);
+
+        Car car1 = new Car();
+        car1.setModel("X8");
+        car1.setManufacturer(manufacturerService.get(2L));
+        car1.setDrivers(List.of(driverService.get(1L), driverService.get(2L)));
+        carService.create(car1);
+        System.out.println("\n car1 created: ");
+        System.out.println(carService.get(car1.getId()));
+
+        Car car2 = new Car();
+        car2.setModel("TT");
+        car2.setManufacturer(manufacturerService.get(1L));
+        car2.setDrivers(List.of(driverService.get(2L), driverService.get(3L)));
+        carService.create(car2);
+        System.out.println("\n car2 created: ");
+        System.out.println(carService.get(car2.getId()));
+
+        Car car3 = new Car();
+        car3.setModel("Model Y");
+        car3.setManufacturer(manufacturerService.get(4L));
+        car3.setDrivers(List.of(driverService.get(2L),
+                driverService.get(3L),
+                driverService.get(4L)));
+        carService.create(car3);
+        System.out.println("\n car3 created: ");
+        System.out.println(carService.get(car3.getId()));
+
+        car3.setModel("Model X");
+        car3.setDrivers(List.of(driverService.get(2L),
+                driverService.get(3L),
+                driverService.get(4L),
+                driverService.get(5L)));
+        carService.update(car3);
+        System.out.println("\n car3 updated: ");
+        System.out.println(carService.get(car3.getId()));
+
+        System.out.println();
+        carService.getAll().forEach(System.out::println);
+
+        carService.delete(1L);
+
+        System.out.println("\n car1 deleted: ");
+        carService.getAll().forEach(System.out::println);
+
+        System.out.println("\n hetAllByDriver: ");
+        carService.getAllByDriver(3L).forEach(System.out::println);
+
+        System.out.println("\n addDriverToCar: ");
+        carService.addDriverToCar(driverService.get(4L), carService.get(2L));
+        carService.getAllByDriver(4L).forEach(System.out::println);
+
+        System.out.println("\n removeDriverFromCar: ");
+        carService.removeDriverFromCar(driverService.get(4L), carService.get(2L));
+        carService.getAllByDriver(4L).forEach(System.out::println);
+
     }
 }
---
+++ src/main/java/mate/jdbc/service/impl/CarServiceImpl.java
@@ -0,0 +1,74 @@
+package mate.jdbc.service.impl;
+
+import java.util.List;
+import mate.jdbc.dao.CarDao;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.lib.Service;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.CarService;
+
+@Service
+public class CarServiceImpl implements CarService {
+    @Inject
+    private CarDao carDao;
+
+    @Override
+    public Car create(Car car) {
+        return carDao.create(car);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return carDao.delete(id);
+    }
+
+    @Override
+    public Car update(Car car) {
+        if (carDao.update(car)) {
+            return car;
+        } else {
+            throw new RuntimeException("Can't update car " + car);
+        }
+    }
+
+    @Override
+    public Car get(Long id) {
+        return carDao.get(id).orElseThrow(
+                () -> new RuntimeException("Can't get car by id: " + id));
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return carDao.getAll();
+    }
+
+    @Override
+    public List<Car> getAllByDriver(Long driverId) {
+        return carDao.getAllByDriver(driverId);
+    }
+
+    @Override
+    public void addDriverToCar(Driver driver, Car car) {
+        List<Driver> carDriverList = car.getDrivers();
+        if (carDriverList.get(0).getId() == null) {
+            car.setDrivers(List.of(driver));
+        } else if (!carDriverList.contains(driver)) {
+            carDriverList.add(driver);
+        } else {
+            throw new RuntimeException("Driver: " + driver + " is already in car: " + car);
+        }
+        carDao.update(car);
+    }
+
+    @Override
+    public void removeDriverFromCar(Driver driver, Car car) {
+        List<Driver> carDriverList = car.getDrivers();
+        if (carDriverList.contains(driver)) {
+            carDriverList.remove(driver);
+        } else {
+            throw new RuntimeException("Driver: " + driver + " is not in car: " + car);
+        }
+        carDao.update(car);
+    }
+}
